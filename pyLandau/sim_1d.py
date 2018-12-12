45962d80a97de39d5ad7ead076bde1391bda8f0d
JonasSuni <jonas.suni@helsinki.fi>
2018-12-12 15:20:48 +0200

    nvT simulator now functional, but untested. All simulators now true scale, but more unstable

--- pyLandau/sim_1d.py
+++ pyLandau/sim_1d.py
@@ -5,19 +5,41 @@ import scipy.constants as sc
 
 def divr(flux,r):
 
-    return np.gradient(flux)/(r**2)
+    #return divr_false(flux,r)
+    return divr_true(flux,r)
+
+def grad(f):
+
+    #return grad_false(f)
+    return grad_true(f)
+
+def grad_false(f):
+
+    return (2*f-np.append(f[0],f)[:-1]-np.append(f,f[-1])[1:])/2
+
+def grad_true(f):
+
+    return np.gradient(f,695e+6)
+
+def divr_true(flux,r):
+
+    return np.gradient(flux,695e+6)/(r**2)
+
+def divr_false(flux,r):
+
+    return (2*flux-np.append(flux[0],flux)[:-1]-np.append(flux,flux[-1])[1:])/(2*r**2)
 
 def fit_n(r,a1,a2):
 
-    return a1*(r**a2)
+    return a1*((r/2)**a2)
 
 def fit_v(r,a1,a2,a3):
 
-    return a1+a2*(np.log(r))**a3
+    return a1+a2*(np.log(r/2))**a3
 
-def sim_n_rk4(n0=100.0,v0=1,t=10000,dt=0.1,rmax=10,dr=0.01):
+def sim_n_rk4(n0=100.0,v0=1,t=10000,dt=0.1,rmax=10,dr=0.01,figname="unnamed",animate=True,cpf=1000):
 
-    r = np.arange(1,rmax,dr).astype(float)
+    r = np.arange(2,rmax,dr).astype(float)
 
     n = np.zeros_like(r)
     n[0]=n0
@@ -31,10 +53,11 @@ def sim_n_rk4(n0=100.0,v0=1,t=10000,dt=0.1,rmax=10,dr=0.01):
     ax = fig.add_subplot(111)
     ax.set_xlabel("r",fontsize=20,labelpad=10)
     ax.set_ylabel("n", fontsize=20,labelpad=10)
-    ax.tick_params(labelsize=20)
+    ax.tick_params(labelsize=15)
     plt.grid()
+    if animate:
+        line1, = ax.plot(r,n,"x")
     plt.tight_layout()
-    line1, = ax.plot(r,n,"x")
 
     while not stop:
 
@@ -46,40 +69,45 @@ def sim_n_rk4(n0=100.0,v0=1,t=10000,dt=0.1,rmax=10,dr=0.01):
         n = n + (k1+2*k2+2*k3+k4)/6
         n[0] = n0
 
-        if i%100 == 0:
+        if i%cpf == 0 and animate:
             line1.set_ydata(n)
             fig.canvas.draw()
             fig.canvas.flush_events()
 
         i += 1
 
-        if max(np.abs(k1+2*k2+2*k3+k46)/6) < 0.001*n0*dt:
-            line1.set_ydata(n)
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-            stop = True
-
-        if i>t:
-            line1.set_ydata(n)
-            fig.canvas.draw()
-            fig.canvas.flush_events()
+        if i>t/dt:
+            if animate:
+                line1.set_ydata(n)
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+            else:
+                ax.plot(r,n,"x")
             stop = True
 
     popt,pcov = scipy.optimize.curve_fit(fit_n,r,n,p0=[n0,-2])
-    y = fit_sim(r,popt[0],popt[1])
+    y = fit_n(r,popt[0],popt[1])
     ax.plot(r,y,color="red")
     fig.show()
     print(popt)
 
+    fig.savefig("landau/simulations/sim_n_rk4_"+figname+".png")
+
+    f = open("landau/simulations/sim_n_rk4_pars_"+figname+".txt","w")
+    f.write(str(popt[0])+" "+str(popt[1]))
+    f.close()
+
     return n
 
-def sim_nv_rk4(n0=100,v0=100,T0=1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed"):
+def sim_nv_rk4(n0=100,v0=100,T0=1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed",animate=True,cpf=1000):
 
-    r = np.arange(1,rmax,dr).astype(float)
+    r = np.arange(2,rmax,dr).astype(float)
     
-    n = n0*(r**-2)
-    #v = np.zeros_like(r)+v0
-    v = 2*T0*(np.log(r)**0.5)+v0
+    T0 = T0*(sc.k/sc.m_p)
+
+    n = n0*((r/r[0])**-2)
+    v = np.zeros_like(r)+v0
+    v = 85000*(np.log(r/r[0])**0.5)+v0
     n[0] = n0
     v[0] = v0
     
@@ -97,25 +125,27 @@ def sim_nv_rk4(n0=100,v0=100,T0=1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed")
     ax2.set_xlabel("r",fontsize=20,labelpad=10)
     ax1.set_ylabel("n",fontsize=20,labelpad=10)
     ax2.set_ylabel("v",fontsize=20,labelpad=10)
-    ax1.tick_params(labelsize=10)
-    ax2.tick_params(labelsize=10)
-    line1, = ax1.plot(r,n,"x")
-    line2, = ax2.plot(r,v,"x")
+    ax1.tick_params(labelsize=15)
+    ax2.tick_params(labelsize=15)
+    ax2.set_ylim(v0,5*v0)
+    if animate:
+        line1, = ax1.plot(r,n,"x")
+        line2, = ax2.plot(r,v,"x")
     plt.tight_layout()
     
     while not stop:
     
         k1n = dt*(-divr(v*n*r**2,r))
-        k1v = dt*(-v*np.gradient(v)-T0*np.gradient(n)/n)
+        k1v = dt*(-v*grad(v)-T0*grad(n)/n)
         
         k2n = dt*(-divr((v+k1v/2)*(n+k1n/2)*r**2,r))
-        k2v = dt*(-(v+k1v/2)*np.gradient(v+k1v/2)-T0*np.gradient(n+k1n/2)/(n+k1n/2))
+        k2v = dt*(-(v+k1v/2)*grad(v+k1v/2)-T0*grad(n+k1n/2)/(n+k1n/2))
         
         k3n = dt*(-divr((v+k2v/2)*(n+k2n/2)*r**2,r))
-        k3v = dt*(-(v+k2v/2)*np.gradient(v+k2v/2)-T0*np.gradient(n+k2n/2)/(n+k2n/2))
+        k3v = dt*(-(v+k2v/2)*grad(v+k2v/2)-T0*grad(n+k2n/2)/(n+k2n/2))
         
         k4n = dt*(-divr((v+k3v)*(n+k3n)*r**2,r))
-        k4v = dt*(-(v+k3v)*np.gradient(v+k3v)-T0*np.gradient(n+k3n)/(n+k3n))
+        k4v = dt*(-(v+k3v)*grad(v+k3v)-T0*grad(n+k3n)/(n+k3n))
         
         n = n + (k1n+2*k2n+2*k3n+k4n)/6
         n[0] = n0
@@ -125,29 +155,24 @@ def sim_nv_rk4(n0=100,v0=100,T0=1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed")
         
         i += 1
         
-        if i%100 == 0:
+        if i%cpf == 0 and animate:
             line1.set_ydata(n)
             line2.set_ydata(v)
             fig.canvas.draw()
             fig.canvas.flush_events()
             
-        if max(np.abs(k1v+2*k2v+2*k3v+k4v)/6) < 0.0001*dt*T0*v0:
+        if i>t/dt:
             stop = True
-            line1.set_ydata(n)
-            line2.set_ydata(v)
-            ax2.set_ylim(v0,max(v))
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-            
-            
-        if i>t:
-            stop = True
-            line1.set_ydata(n)
-            line2.set_ydata(v)
-            ax2.set_ylim(v0,max(v))
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-    
+            if animate:
+                line1.set_ydata(n)
+                line2.set_ydata(v)
+                ax2.set_ylim(v0,max(v))
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+            else:
+                ax1.plot(r,n,"x")
+                ax2.plot(r,v,"x")
+
     plt.tight_layout()
 
     popt,pcov = scipy.optimize.curve_fit(fit_n,r,n,p0=[n0,-2])
@@ -170,12 +195,14 @@ def sim_nv_rk4(n0=100,v0=100,T0=1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed")
     
     return [n,v]
 
-def sim_nvT_rk4(n0=100,v0=100,T0=0.1,qpar=0.1,qperp=0.1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed"):
+def sim_nvT_rk4(n0=100,v0=100,T0=0.1,qpar=0.1,qperp=0.1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed",animate=True,cpf=1000):
 
-    r = np.arange(1,rmax,dr).astype(float)
+    r = np.arange(2,rmax,dr).astype(float)
+
+    T0 = T0*(sc.k/sc.m_p)
     
-    n = n0*(r**-2)
-    v = (5.0/3)*T0*(np.log(r)**0.5)+v0
+    n = n0*((r/r[0])**-2)
+    v = (5.0/3)*T0*(np.log(r/r[0])**0.5)+v0
     Tpar = np.zeros_like(r)+T0
     Tperp = np.zeros_like(r)+T0
     n[0] = n0
@@ -203,37 +230,38 @@ def sim_nvT_rk4(n0=100,v0=100,T0=0.1,qpar=0.1,qperp=0.1,t=10000,dt=1,rmax=10,dr=
     ax2.set_ylabel("v",fontsize=20,labelpad=10)
     ax3.set_ylabel("Tpar",fontsize=20,labelpad=10)
     ax4.set_ylabel("Tperp",fontsize=20,labelpad=10)
-    ax1.tick_params(labelsize=10)
-    ax2.tick_params(labelsize=10)
-    ax3.tick_params(labelsize=10)
-    ax4.tick_params(labelsize=10)
-    line1, = ax1.plot(r,n,"x")
-    line2, = ax2.plot(r,v,"x")
-    line3, = ax3.plot(r,Tpar,"x")
-    line4, = ax4.plot(r,Tperp,"x")
+    ax1.tick_params(labelsize=15)
+    ax2.tick_params(labelsize=15)
+    ax3.tick_params(labelsize=15)
+    ax4.tick_params(labelsize=15)
+    if animate:
+        line1, = ax1.plot(r,n,"x")
+        line2, = ax2.plot(r,v,"x")
+        line3, = ax3.plot(r,Tpar,"x")
+        line4, = ax4.plot(r,Tperp,"x")
     plt.tight_layout()
     
     while not stop:
     
         k1n = dt*(-divr(v*n*r**2,r))
-        k1v = dt*(-v*np.gradient(v)-Tpar*np.gradient(n)/n)
-        k1T1 = -dt*(divr(v*n*Tpar*r**2,r)+2*n*Tpar*np.gradient(v))
-        k1T2 = -dt*(divr(v*n*Tperp*r**2,r)+n*Tperp*divr(v,r)-n*Tperp*np.gradient(v))
+        k1v = dt*(-v*grad(v)-Tpar*grad(n)/n)
+        k1T1 = -dt*(divr(v*n*Tpar*r**2,r)+2*n*Tpar*grad(v))
+        k1T2 = -dt*(divr(v*n*Tperp*r**2,r)+n*Tperp*divr(v,r)-n*Tperp*grad(v))
         
         k2n = dt*(-divr((v+k1v/2)*(n+k1n/2)*r**2,r))
-        k2v = dt*(-(v+k1v/2)*np.gradient(v+k1v/2)-(Tpar+k1T1/2)*np.gradient(n+k1n/2)/(n+k1n/2))
-        k2T1 = 
-        k2T2 = 
+        k2v = dt*(-(v+k1v/2)*grad(v+k1v/2)-(Tpar+k1T1/2)*grad(n+k1n/2)/(n+k1n/2))
+        k2T1 = -dt*(divr((v+k1v/2)*(n+k1n/2)*(Tpar+k1T1/2)*r**2,r)+2*(n+k1n/2)*(Tpar+k1T1/2)*grad(v+k1v/2))
+        k2T2 = -dt*(divr((v+k1v/2)*(n+k1n/2)*(Tperp+k1T2/2)*r**2,r)+(n+k1n/2)*(Tperp+k1T2/2)*divr((v+k1v/2),r)-(n+k1n/2)*(Tperp+k1T2/2)*grad(v+k1v/2))
 
         k3n = dt*(-divr((v+k2v/2)*(n+k2n/2)*r**2,r))
-        k3v = dt*(-(v+k2v/2)*np.gradient(v+k2v/2)-(Tpar+k2T1/2)*np.gradient(n+k2n/2)/(n+k2n/2))
-        k3T1 = 
-        k3T2 = 
+        k3v = dt*(-(v+k2v/2)*grad(v+k2v/2)-(Tpar+k2T1/2)*grad(n+k2n/2)/(n+k2n/2))
+        k3T1 = -dt*(divr((v+k2v/2)*(n+k2n/2)*(Tpar+k2T1/2)*r**2,r)+2*(n+k2n/2)*(Tpar+k2T1/2)*grad(v+k2v/2))
+        k3T2 = -dt*(divr((v+k2v/2)*(n+k2n/2)*(Tperp+k2T2/2)*r**2,r)+(n+k2n/2)*(Tperp+k2T2/2)*divr((v+k2v/2),r)-(n+k2n/2)*(Tperp+k2T2/2)*grad(v+k2v/2))
 
         k4n = dt*(-divr((v+k3v)*(n+k3n)*r**2,r))
-        k4v = dt*(-(v+k3v)*np.gradient(v+k3v)-(Tpar+k3T1)*np.gradient(n+k3n)/(n+k3n))
-        k4T1 = 
-        k4T2 = 
+        k4v = dt*(-(v+k3v)*grad(v+k3v)-(Tpar+k3T1)*grad(n+k3n)/(n+k3n))
+        k4T1 = -dt*(divr((v+k3v)*(n+k3n)*(Tpar+k3T1)*r**2,r)+2*(n+k3n)*(Tpar+k3T1)*grad(v+k3v))
+        k4T2 = -dt*(divr((v+k3v)*(n+k3n)*(Tperp+k3T2)*r**2,r)+(n+k3n)*(Tperp+k3T2)*divr((v+k3v),r)-(n+k3n)*(Tperp+k3T2)*grad(v+k3v))
 
         n = n + (k1n+2*k2n+2*k3n+k4n)/6
         n[0] = n0
@@ -249,7 +277,7 @@ def sim_nvT_rk4(n0=100,v0=100,T0=0.1,qpar=0.1,qperp=0.1,t=10000,dt=1,rmax=10,dr=
 
         i += 1
         
-        if i%100 == 0:
+        if i%cpf == 0 and animate:
             line1.set_ydata(n)
             line2.set_ydata(v)
             line3.set_ydata(Tpar)
@@ -257,31 +285,24 @@ def sim_nvT_rk4(n0=100,v0=100,T0=0.1,qpar=0.1,qperp=0.1,t=10000,dt=1,rmax=10,dr=
             fig.canvas.draw()
             fig.canvas.flush_events()
             
-        if max(np.abs(k1v+2*k2v+2*k3v+k4v)/6) < 0.01*dt*T0*v0:
+        if i>t/dt:
             stop = True
-            line1.set_ydata(n)
-            line2.set_ydata(v)
-            line3.set_ydata(Tpar)
-            line4.set_ydata(Tperp)
-            ax2.set_ylim(v0,max(v))
-            ax3.set_ylim(min(Tpar),max(Tpar))
-            ax4.set_ylim(min(Tperp),max(Tperp))
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-            
-            
-        if i>t:
-            stop = True
-            line1.set_ydata(n)
-            line2.set_ydata(v)
-            line3.set_ydata(Tpar)
-            line4.set_ydata(Tperp)
-            ax2.set_ylim(v0,max(v))
-            ax3.set_ylim(min(Tpar),max(Tpar))
-            ax4.set_ylim(min(Tperp),max(Tperp))
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-    
+            if animate:
+                line1.set_ydata(n)
+                line2.set_ydata(v)
+                line3.set_ydata(Tpar)
+                line4.set_ydata(Tperp)
+                ax2.set_ylim(v0,max(v))
+                ax3.set_ylim(min(Tpar),max(Tpar))
+                ax4.set_ylim(min(Tperp),max(Tperp))
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+            else:
+                ax1.plot(r,n,"x")
+                ax2.plot(r,v,"x")
+                ax3.plot(r,Tpar,"x")
+                ax4.plot(r,Tperp,"x")
+
     plt.tight_layout()
 
     popt,pcov = scipy.optimize.curve_fit(fit_n,r,n,p0=[n0,-2])
