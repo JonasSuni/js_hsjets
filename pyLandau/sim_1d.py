7e69613248338ccbac09572957b3c4a3dd9c47b1
JonasSuni <jonas.suni@helsinki.fi>
2018-12-13 15:00:56 +0200

    Added gravitation term, simulators now physical, but nV unstable and nvT not functional

--- pyLandau/sim_1d.py
+++ pyLandau/sim_1d.py
@@ -15,7 +15,7 @@ def grad(f):
 
 def grad_false(f):
 
-    return (2*f-np.append(f[0],f)[:-1]-np.append(f,f[-1])[1:])/2
+    return (2*f-np.append(f[0],f)[:-1]-np.append(f,f[-1])[1:])/(2*695e+6)
 
 def grad_true(f):
 
@@ -27,7 +27,7 @@ def divr_true(flux,r):
 
 def divr_false(flux,r):
 
-    return (2*flux-np.append(flux[0],flux)[:-1]-np.append(flux,flux[-1])[1:])/(2*r**2)
+    return (2*flux-np.append(flux[0],flux)[:-1]-np.append(flux,flux[-1])[1:])/(2*695e+6*r**2)
 
 def fit_n(r,a1,a2):
 
@@ -40,6 +40,7 @@ def fit_v(r,a1,a2,a3):
 def sim_n_rk4(n0=100.0,v0=1,t=10000,dt=0.1,rmax=10,dr=0.01,figname="unnamed",animate=True,cpf=1000):
 
     r = np.arange(2,rmax,dr).astype(float)
+    rt = r*695e+6
 
     n = np.zeros_like(r)
     n[0]=n0
@@ -61,10 +62,10 @@ def sim_n_rk4(n0=100.0,v0=1,t=10000,dt=0.1,rmax=10,dr=0.01,figname="unnamed",ani
 
     while not stop:
 
-        k1 = dt*(-divr(v0*n*r**2,r))
-        k2 = dt*(-divr(v0*(n+k1/2)*r**2,r))
-        k3 = dt*(-divr(v0*(n+k2/2)*r**2,r))
-        k4 = dt*(-divr(v0*(n+k3)*r**2,r))
+        k1 = dt*(-divr(v0*n*rt**2,rt))
+        k2 = dt*(-divr(v0*(n+k1/2)*rt**2,rt))
+        k3 = dt*(-divr(v0*(n+k2/2)*rt**2,rt))
+        k4 = dt*(-divr(v0*(n+k3)*rt**2,rt))
 
         n = n + (k1+2*k2+2*k3+k4)/6
         n[0] = n0
@@ -102,12 +103,16 @@ def sim_n_rk4(n0=100.0,v0=1,t=10000,dt=0.1,rmax=10,dr=0.01,figname="unnamed",ani
 def sim_nv_rk4(n0=100,v0=100,T0=1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed",animate=True,cpf=1000):
 
     r = np.arange(2,rmax,dr).astype(float)
+
+    rt = r*695e+6
     
     T0 = T0*(sc.k/sc.m_p)
+    GS = sc.m_p*sc.G*1.988e+30
 
     n = n0*((r/r[0])**-2)
+    #n = (-n0/218)*(r-r[0])+n0
     v = np.zeros_like(r)+v0
-    v = 85000*(np.log(r/r[0])**0.5)+v0
+    #v = 90000*(np.log(r/r[0])**0.5)+v0
     n[0] = n0
     v[0] = v0
     
@@ -127,7 +132,7 @@ def sim_nv_rk4(n0=100,v0=100,T0=1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed",
     ax2.set_ylabel("v",fontsize=20,labelpad=10)
     ax1.tick_params(labelsize=15)
     ax2.tick_params(labelsize=15)
-    ax2.set_ylim(v0,5*v0)
+    ax2.set_ylim(0,5*v0)
     if animate:
         line1, = ax1.plot(r,n,"x")
         line2, = ax2.plot(r,v,"x")
@@ -135,17 +140,17 @@ def sim_nv_rk4(n0=100,v0=100,T0=1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed",
     
     while not stop:
     
-        k1n = dt*(-divr(v*n*r**2,r))
-        k1v = dt*(-v*grad(v)-T0*grad(n)/n)
+        k1n = dt*(-divr(v*n*rt**2,rt))
+        k1v = dt*(-v*grad(v)-T0*grad(n)/n-GS*n/(rt**2))
         
-        k2n = dt*(-divr((v+k1v/2)*(n+k1n/2)*r**2,r))
-        k2v = dt*(-(v+k1v/2)*grad(v+k1v/2)-T0*grad(n+k1n/2)/(n+k1n/2))
+        k2n = dt*(-divr((v+k1v/2)*(n+k1n/2)*rt**2,rt))
+        k2v = dt*(-(v+k1v/2)*grad(v+k1v/2)-T0*grad(n+k1n/2)/(n+k1n/2)-GS*(n+k1n/2)/(rt**2))
         
-        k3n = dt*(-divr((v+k2v/2)*(n+k2n/2)*r**2,r))
-        k3v = dt*(-(v+k2v/2)*grad(v+k2v/2)-T0*grad(n+k2n/2)/(n+k2n/2))
+        k3n = dt*(-divr((v+k2v/2)*(n+k2n/2)*rt**2,rt))
+        k3v = dt*(-(v+k2v/2)*grad(v+k2v/2)-T0*grad(n+k2n/2)/(n+k2n/2)-GS*(n+k2n/2)/(rt**2))
         
-        k4n = dt*(-divr((v+k3v)*(n+k3n)*r**2,r))
-        k4v = dt*(-(v+k3v)*grad(v+k3v)-T0*grad(n+k3n)/(n+k3n))
+        k4n = dt*(-divr((v+k3v)*(n+k3n)*rt**2,rt))
+        k4v = dt*(-(v+k3v)*grad(v+k3v)-T0*grad(n+k3n)/(n+k3n)-GS*(n+k3n)/(rt**2))
         
         n = n + (k1n+2*k2n+2*k3n+k4n)/6
         n[0] = n0
@@ -198,8 +203,10 @@ def sim_nv_rk4(n0=100,v0=100,T0=1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed",
 def sim_nvT_rk4(n0=100,v0=100,T0=0.1,qpar=0.1,qperp=0.1,t=10000,dt=1,rmax=10,dr=0.1,figname="unnamed",animate=True,cpf=1000):
 
     r = np.arange(2,rmax,dr).astype(float)
+    rt = r*695e+6
 
-    T0 = T0*(sc.k/sc.m_p)
+    T0 = T0*sc.k
+    GS = sc.m_p*sc.G*1.988e+30
     
     n = n0*((r/r[0])**-2)
     v = (5.0/3)*T0*(np.log(r/r[0])**0.5)+v0
@@ -219,7 +226,7 @@ def sim_nvT_rk4(n0=100,v0=100,T0=0.1,qpar=0.1,qperp=0.1,t=10000,dt=1,rmax=10,dr=
     ax1 = fig.add_subplot(221)
     ax2 = fig.add_subplot(222)
     ax3 = fig.add_subplot(223)
-    ax3 = fig.add_subplot(224)
+    ax4 = fig.add_subplot(224)
     ax1.grid(b=True)
     ax2.grid(b=True)
     ax3.grid(b=True)
@@ -243,25 +250,25 @@ def sim_nvT_rk4(n0=100,v0=100,T0=0.1,qpar=0.1,qperp=0.1,t=10000,dt=1,rmax=10,dr=
     
     while not stop:
     
-        k1n = dt*(-divr(v*n*r**2,r))
-        k1v = dt*(-v*grad(v)-Tpar*grad(n)/n)
-        k1T1 = -dt*(divr(v*n*Tpar*r**2,r)+2*n*Tpar*grad(v))
-        k1T2 = -dt*(divr(v*n*Tperp*r**2,r)+n*Tperp*divr(v,r)-n*Tperp*grad(v))
+        k1n = dt*(-divr(v*n*rt**2,rt))
+        k1v = dt*(-v*grad(v)-grad(Tpar*n)/(sc.m_p*n)-GS*n/(rt**2))
+        k1T1 = -dt*(divr(v*n*Tpar*rt**2,rt)+2*n*Tpar*grad(v))
+        k1T2 = -dt*(divr(v*n*Tperp*rt**2,rt)+n*Tperp*divr(v,rt)-n*Tperp*grad(v))
         
-        k2n = dt*(-divr((v+k1v/2)*(n+k1n/2)*r**2,r))
-        k2v = dt*(-(v+k1v/2)*grad(v+k1v/2)-(Tpar+k1T1/2)*grad(n+k1n/2)/(n+k1n/2))
-        k2T1 = -dt*(divr((v+k1v/2)*(n+k1n/2)*(Tpar+k1T1/2)*r**2,r)+2*(n+k1n/2)*(Tpar+k1T1/2)*grad(v+k1v/2))
-        k2T2 = -dt*(divr((v+k1v/2)*(n+k1n/2)*(Tperp+k1T2/2)*r**2,r)+(n+k1n/2)*(Tperp+k1T2/2)*divr((v+k1v/2),r)-(n+k1n/2)*(Tperp+k1T2/2)*grad(v+k1v/2))
+        k2n = dt*(-divr((v+k1v/2)*(n+k1n/2)*rt**2,rt))
+        k2v = dt*(-(v+k1v/2)*grad(v+k1v/2)-grad((Tpar+k1T1/2)*(n+k1n/2))/(sc.m_p*(n+k1n/2))-GS*(n+k1n/2)/(rt**2))
+        k2T1 = -dt*(divr((v+k1v/2)*(n+k1n/2)*(Tpar+k1T1/2)*rt**2,rt)+2*(n+k1n/2)*(Tpar+k1T1/2)*grad(v+k1v/2))
+        k2T2 = -dt*(divr((v+k1v/2)*(n+k1n/2)*(Tperp+k1T2/2)*rt**2,rt)+(n+k1n/2)*(Tperp+k1T2/2)*divr((v+k1v/2),rt)-(n+k1n/2)*(Tperp+k1T2/2)*grad(v+k1v/2))
 
-        k3n = dt*(-divr((v+k2v/2)*(n+k2n/2)*r**2,r))
-        k3v = dt*(-(v+k2v/2)*grad(v+k2v/2)-(Tpar+k2T1/2)*grad(n+k2n/2)/(n+k2n/2))
-        k3T1 = -dt*(divr((v+k2v/2)*(n+k2n/2)*(Tpar+k2T1/2)*r**2,r)+2*(n+k2n/2)*(Tpar+k2T1/2)*grad(v+k2v/2))
-        k3T2 = -dt*(divr((v+k2v/2)*(n+k2n/2)*(Tperp+k2T2/2)*r**2,r)+(n+k2n/2)*(Tperp+k2T2/2)*divr((v+k2v/2),r)-(n+k2n/2)*(Tperp+k2T2/2)*grad(v+k2v/2))
+        k3n = dt*(-divr((v+k2v/2)*(n+k2n/2)*rt**2,rt))
+        k3v = dt*(-(v+k2v/2)*grad(v+k2v/2)-grad((Tpar+k2T1/2)*(n+k2n/2))/(sc.m_p*(n+k2n/2))-GS*(n+k2n/2)/(rt**2))
+        k3T1 = -dt*(divr((v+k2v/2)*(n+k2n/2)*(Tpar+k2T1/2)*rt**2,rt)+2*(n+k2n/2)*(Tpar+k2T1/2)*grad(v+k2v/2))
+        k3T2 = -dt*(divr((v+k2v/2)*(n+k2n/2)*(Tperp+k2T2/2)*rt**2,rt)+(n+k2n/2)*(Tperp+k2T2/2)*divr((v+k2v/2),rt)-(n+k2n/2)*(Tperp+k2T2/2)*grad(v+k2v/2))
 
-        k4n = dt*(-divr((v+k3v)*(n+k3n)*r**2,r))
-        k4v = dt*(-(v+k3v)*grad(v+k3v)-(Tpar+k3T1)*grad(n+k3n)/(n+k3n))
-        k4T1 = -dt*(divr((v+k3v)*(n+k3n)*(Tpar+k3T1)*r**2,r)+2*(n+k3n)*(Tpar+k3T1)*grad(v+k3v))
-        k4T2 = -dt*(divr((v+k3v)*(n+k3n)*(Tperp+k3T2)*r**2,r)+(n+k3n)*(Tperp+k3T2)*divr((v+k3v),r)-(n+k3n)*(Tperp+k3T2)*grad(v+k3v))
+        k4n = dt*(-divr((v+k3v)*(n+k3n)*rt**2,rt))
+        k4v = dt*(-(v+k3v)*grad(v+k3v)-grad((Tpar+k3T1)*(n+k3n))/(sc.m_p*(n+k3n))-GS*(n+k2n/2)/(rt**2))
+        k4T1 = -dt*(divr((v+k3v)*(n+k3n)*(Tpar+k3T1)*rt**2,rt)+2*(n+k3n)*(Tpar+k3T1)*grad(v+k3v))
+        k4T2 = -dt*(divr((v+k3v)*(n+k3n)*(Tperp+k3T2)*rt**2,rt)+(n+k3n)*(Tperp+k3T2)*divr((v+k3v),rt)-(n+k3n)*(Tperp+k3T2)*grad(v+k3v))
 
         n = n + (k1n+2*k2n+2*k3n+k4n)/6
         n[0] = n0
