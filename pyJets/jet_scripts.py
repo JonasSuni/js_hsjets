0ca558ec345b41865b386ffcdf1bef021193e6ff
JonasSuni <jonas.suni@helsinki.fi>
2019-10-22 12:18:21 +0300

    Fix 7

--- pyJets/jet_scripts.py
+++ pyJets/jet_scripts.py
@@ -1994,7 +1994,7 @@ def hack_2019_fig35():
     bs_y = np.arange(-10,10,0.01)
     mp_p,bs_p = ja.bs_mp_fit("AEA",1320,[5,20,-10,10])
     bs_x = np.polyval(bs_p,bs_y)
-    mp_x = np.polyval(mp_p,bs_y)+1
+    mp_x = np.polyval(mp_p,bs_y)+2
 
     fig,ax = plt.subplots(1,1,figsize=(10,10))
 
