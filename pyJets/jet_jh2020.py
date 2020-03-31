251570a9d541669df20bd0dd50adcbd8a59a381d
JonasSuni <jonas.suni@helsinki.fi>
2020-03-31 13:21:33 +0300

    Fix?

--- pyJets/jet_jh2020.py
+++ pyJets/jet_jh2020.py
@@ -476,6 +476,9 @@ def separate_jets(runid):
             except:
                 continue
 
+            print(jet_first_cells)
+            print(np.intersect1d(jet_first_cells,matched_cells))
+
             if np.intersect1d(jet_first_cells,matched_cells).size > 0.25*len(jet_first_cells):
                 sj_jet_ids.append(n1)
                 break
