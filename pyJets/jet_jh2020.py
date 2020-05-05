a3c7d77579529fc1bf4a3a33f72820c542e0cc12
JonasSuni <jonas.suni@helsinki.fi>
2020-05-05 15:26:11 +0300

    Fix

--- pyJets/jet_jh2020.py
+++ pyJets/jet_jh2020.py
@@ -743,7 +743,7 @@ def jh2020_fig1(var="pdyn"):
 
     pt.plot.plot_colormap(filename=filepath,outputfile=outputdir+"fig1b_{}.png".format(var),boxre=[6,18,-6,6],usesci=0,lin=1,expression=expr_list[var_index],vmin=0,vmax=vmax_list[var_index],colormap=colmap,cbtitle=label_list[var_index],external=jh20f1_ext,pass_vars=["rho","v","CellID","Pdyn","RhoNonBackstream","PTensorNonBackstreamDiagonal","Mmsx","B","core_heating"])
 
-def jh2020_movie(runid,start,stop,var="pdyn",arr_draw=False,debug=False):
+def jh2020_movie(runid,start,stop,var="Pdyn",arr_draw=False,debug=False):
 
     runid_list = ["ABA","ABC","AEA","AEC"]
     maxfnr_list = [839,1179,1339,879]
