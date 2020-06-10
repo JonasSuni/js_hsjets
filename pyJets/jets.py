51dd77053ecac8e5bd3859c384342ed7b7f615f6
JonasSuni <jonas.suni@helsinki.fi>
2020-06-10 16:58:23 +0300

    Fix

--- pyJets/jets.py
+++ pyJets/jets.py
@@ -5,7 +5,7 @@ Imports all the modules in the pyJets folder
 from jet_analyser import ci2vars,ci2vars_nofile,read_mult_vars,xyz_reconstruct,restrict_area,bow_shock_markus,sw_normalisation,bs_mp_fit,make_cust_mask_opt,make_cust_mask_opt_new
 #from jet_contours import *
 from jetfile_make import pahkmake
-from jet_io import PropReader,jet_maker,track_jets,calc_event_props,sort_jets_2,eventfile_read,get_sheath_cells,get_neighbors,jet_creator,jet_tracker
+from jet_io import PropReader,jet_maker,track_jets,calc_event_props,sort_jets_2,eventfile_read,get_sheath_cells,get_neighbors,jet_creator,jet_tracker,get_neighbors_asym
 from jet_scripts import jet_pos_graph,jet_paper_pos,slamjet_plotter, jet_paper_counter,jet_plotter,MMSReader,hack_2019_fig4,jetcand_vdf,hack_2019_fig6,read_mult_runs,DT_comparison,hack_2019_fig1,hack_2019_fig2,DT_mach_comparison,hack_2019_fig6_alt,hack_2019_fig78,hack_2019_fig9,MMSJet,get_timeseries,hack_2019_fig35,slams_jet_counter,make_transient_timeseries,h19_extra_1,get_SEA,plot_new_sj
 from plot_contours import expr_pdyn_gen,expr_pdyn,expr_srho
 from tavg_maker import avg_maker_slow,TP_maker
