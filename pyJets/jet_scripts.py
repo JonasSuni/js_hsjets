b96642e1ac0da58b6ed86097f44b077a2f41e460
JonasSuni <jonas.suni@helsinki.fi>
2020-09-02 14:24:21 +0300

    Puhti test

--- pyJets/jet_scripts.py
+++ pyJets/jet_scripts.py
@@ -952,7 +952,8 @@ def rev1_jetpath(runid,vavgfilename,time_thresh=5,crop=True):
 
     bulkpath = jx.find_bulkpath(runid)
 
-    vlsvobj = pt.vlsvfile.VlsvReader(bulkpath+"bulk.0001000.vlsv")
+    #vlsvobj = pt.vlsvfile.VlsvReader(bulkpath+"bulk.0001000.vlsv")
+    vlsvobj = pt.vlsvfile.VlsvReader("/scratch/project_2000506/sunijona/bulks/"+runid+"/bulk.0001000.vlsv")
     vavg = np.load(wrkdir_DNR+"tavg/velocities/"+runid+"/"+vavgfilename)
     vavgx = vavg[:,0]/r_e
     vavgy = vavg[:,1]/r_e
