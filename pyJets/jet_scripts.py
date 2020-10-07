957a62707127c55c43f50df6a68134a236c50ecb
JonasSuni <jonas.suni@helsinki.fi>
2020-10-07 18:45:03 +0300

    Tewst

--- pyJets/jet_scripts.py
+++ pyJets/jet_scripts.py
@@ -1185,8 +1185,8 @@ def rev1_defplot(time_thresh=5,type="angmag"):
         ax_list[0].axhline(0,linestyle="dashed",color="black",zorder=2)
         #ax_list[0].set_ylabel("$\mathrm{Deflection~[kms^{-1}]}$",fontsize=20)
         #ax_list[1].set_ylabel("$\mathrm{Deflection~angle~[deg]}$",fontsize=20)
-        ax_list[0].set_ylabel("$\mathrm{|v|-\left<|v|\right>~[kms^{-1}]}$",fontsize=20)
-        ax_list[1].set_ylabel("$\mathrm{\\theta-\left<\\theta\right>~[^\circ]}$",fontsize=20)
+        ax_list[0].set_ylabel("$\mathrm{|v|-\\left<|v|\\right>~[kms^{-1}]}$",fontsize=20)
+        ax_list[1].set_ylabel("$\mathrm{\\theta-\\left<\\theta\\right>~[^\circ]}$",fontsize=20)
     elif type == "sheath":
         ax_list[0].set_xlim(-0.5,2.1)
         ax_list[0].set_ylim(0,350)
