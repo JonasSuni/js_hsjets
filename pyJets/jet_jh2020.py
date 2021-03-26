af82a0fd80d9eae664230c7e181d185fe787eb84
JonasSuni <jonas.suni@helsinki.fi>
2021-03-26 14:24:35 +0200

    Test

--- pyJets/jet_jh2020.py
+++ pyJets/jet_jh2020.py
@@ -1044,7 +1044,7 @@ def pendep_hist(runids=["ABA", "ABC", "AEA", "AEC"], panel_one=True):
     plt.close(fig)
 
     print(expfit_pendep(xinterp, sj_popt[0], sj_popt[1])[0])
-    print(sj_hist[0])
+    print(sj_hist[0:5])
 
 
 def expfit_pendep(xdata, a1, a2):
