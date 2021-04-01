743d2de110084146f9f708d51c8499a031b0fe43
JonasSuni <jonas.suni@helsinki.fi>
2021-04-01 11:40:44 +0300

    Fix

--- pyJets/jet_jh2020.py
+++ pyJets/jet_jh2020.py
@@ -1724,8 +1724,8 @@ def jh20f1_ext(ax, XmeshXY, YmeshXY, pass_maps):
             colors=jx.orange,
         )
         ch_cont.collections[0].set_label("$T_{core} = 3T_{sw}$")
-        xbox = [6, 6, 18, 18]
-        ybox = [-6, 6, 6, -6]
+        xbox = [6, 6, 18, 18, 6]
+        ybox = [-6, 6, 6, -6, -6]
         ax.plot(xbox, ybox, color="black", linewidth=0.8 * markscaler)
 
     if draw_arrows:
