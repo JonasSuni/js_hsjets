17c8903dc9f3cfa866171fd1773122e3cc4ee38e
JonasSuni <jonas.suni@helsinki.fi>
2021-06-02 09:52:38 +0300

    Fix

--- pyJets/jet_jh2020.py
+++ pyJets/jet_jh2020.py
@@ -631,14 +631,14 @@ def mag_thresh_plot(allow_splinters=False):
 
         for idx in range(len(runid_list)):
             share_ste[n, idx] = jx.division_ste(
-                data[0, idx] / run_length,
-                data[1, idx] / run_length,
+                data[0, idx] / run_length[idx],
+                data[1, idx] / run_length[idx],
                 data[0 + 3, idx],
                 data[1 + 3, idx],
             )
             slams_share_ste[n, idx] = jx.division_ste(
-                data[0, idx] / run_length,
-                data[2, idx] / run_length,
+                data[0, idx] / run_length[idx],
+                data[2, idx] / run_length[idx],
                 data[0 + 3, idx],
                 data[2 + 3, idx],
             )
