cc6de808ffae69ced60a268d73483b2a6eaad143
JonasSuni <jonas.suni@helsinki.fi>
2021-06-02 13:36:30 +0300

    Test

--- pyJets/jet_jh2020.py
+++ pyJets/jet_jh2020.py
@@ -1022,14 +1022,18 @@ def pendep_hist(runids=["ABA", "ABC", "AEA", "AEC"], panel_one=True):
             color="black",
             label="Non-FCS-jets",
         )
-        ax[1].set_xlabel("$\mathrm{x_{last}-x_{bs}}~[R_e]$", fontsize=25, labelpad=10)
+        ax[1].set_xlabel(
+            "Disappearance distance\n$x_\mathrm{last}-x_\mathrm{bs}~[R_\mathrm{E}]$",
+            fontsize=25,
+            labelpad=10,
+        )
         sj_hist, sj_bins, sj_patch = ax[1].hist(
             sj_pendeps,
             bins=bins,
             weights=sj_weights,
             histtype="step",
             color="red",
-            label="FCS-jets",
+            # label="FCS-jets",
             cumulative=True,
         )
         non_hist, non_bins, non_patch = ax[1].hist(
@@ -1038,14 +1042,18 @@ def pendep_hist(runids=["ABA", "ABC", "AEA", "AEC"], panel_one=True):
             weights=non_weights,
             histtype="step",
             color="black",
-            label="Non-FCS-jets",
+            # label="Non-FCS-jets",
             cumulative=True,
         )
         # ax[0].legend(frameon=False,numpoints=1,markerscale=2,fontsize=15,loc="upper left")
         # ax[1].legend(frameon=False,numpoints=1,markerscale=2,fontsize=15,loc="upper left")
         ax[0].set_ylabel("Jets/s", fontsize=25, labelpad=10)
         ax[1].set_ylabel("Cumulative\nfraction of jets", fontsize=25, labelpad=10)
-        ax[0].set_xlabel("$\mathrm{x_{last}-x_{bs}}~[R_e]$", fontsize=25, labelpad=10)
+        ax[0].set_xlabel(
+            "Disappearance distance\n$x_\mathrm{last}-x_\mathrm{bs}~[R_\mathrm{E}]$",
+            fontsize=25,
+            labelpad=10,
+        )
         ax[0].tick_params(labelsize=15)
         ax[1].tick_params(labelsize=15)
         ax[1].set_ylim(bottom=0)
@@ -1056,14 +1064,18 @@ def pendep_hist(runids=["ABA", "ABC", "AEA", "AEC"], panel_one=True):
     else:
         fig, ax = plt.subplots(1, 1, figsize=(5, 5))
         # plt.grid()
-        ax.set_xlabel("$\mathrm{x_{last}-x_{bs}}~[R_e]$", fontsize=20, labelpad=10)
+        ax.set_xlabel(
+            "Disappearance distance\n$x_\mathrm{last}-x_\mathrm{bs}~[R_\mathrm{E}]$",
+            fontsize=25,
+            labelpad=10,
+        )
         sj_hist, sj_bins, sj_patch = ax.hist(
             sj_pendeps,
             bins=bins,
             weights=sj_weights,
             histtype="step",
             color="red",
-            label="FCS-jets",
+            # label="FCS-jets",
             cumulative=True,
         )
         non_hist, non_bins, non_patch = ax.hist(
@@ -1072,7 +1084,7 @@ def pendep_hist(runids=["ABA", "ABC", "AEA", "AEC"], panel_one=True):
             weights=non_weights,
             histtype="step",
             color="black",
-            label="Non-FCS-jets",
+            # label="Non-FCS-jets",
             cumulative=True,
         )
         # ax.legend(frameon=False,numpoints=1,markerscale=2,fontsize=15,loc="upper left")
@@ -1108,7 +1120,9 @@ def pendep_hist(runids=["ABA", "ABC", "AEA", "AEC"], panel_one=True):
             expfit_pendep(xinterp, sj_popt[0], sj_popt[1]),
             color="red",
             linestyle="dashed",
-            label="EF: {:.2f}Re".format(-1.0 / sj_popt[0]),
+            label="FCS-jets\n$e$-folding: {:.2f}R$_\mathrm{E}$".format(
+                -1.0 / sj_popt[0]
+            ),
         )
         jet_max = non_hist[-1]
         ax[1].plot(
@@ -1116,7 +1130,9 @@ def pendep_hist(runids=["ABA", "ABC", "AEA", "AEC"], panel_one=True):
             expfit_pendep(xinterp, non_popt[0], non_popt[1]),
             color="black",
             linestyle="dashed",
-            label="EF: {:.2f}Re".format(-1.0 / non_popt[0]),
+            label="Non-FCS-jets\n$e$-folding: {:.2f}R$_\mathrm{E}$".format(
+                -1.0 / non_popt[0]
+            ),
         )
         ax[0].annotate("a)", (0.05, 0.1), xycoords="axes fraction", fontsize=20)
         ax[1].annotate("b)", (0.05, 0.1), xycoords="axes fraction", fontsize=20)
