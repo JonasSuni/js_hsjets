f3ced4f82e46b79716581bfd5867dedffff066c9
JonasSuni <jonas.suni@helsinki.fi>
2021-06-29 11:55:46 +0300

    Test

--- pyJets/jet_21_scripts.py
+++ pyJets/jet_21_scripts.py
@@ -35,8 +35,11 @@ homedir = os.environ["HOME"] + "/"
 
 def make_flap_plots():
 
-    for n in range(12, 27, 1):
-        tail_sheet_jplot(xcut=n)
+    # for n in range(12, 27, 1):
+    #    tail_sheet_jplot(xcut=n)
+
+    for n in range(0, 11, 1):
+        tail_sheet_jplot_y(xcut=n)
 
 
 def tail_sheet_jplot(xcut=20):
@@ -79,6 +82,51 @@ def tail_sheet_jplot(xcut=20):
     plt.tight_layout()
     # fig.savefig(wrkdir_DNR + "Figures/sum21/tail_sheet_jplot_x{}.pdf".format(xcut))
     fig.savefig(wrkdir_DNR + "Figures/sum21/tail_sheet_jplot_x{}.png".format(xcut))
+    plt.close(fig)
+
+    return None
+
+
+def tail_sheet_jplot_y(xcut=20):
+
+    fnr_range = np.arange(1200, 1501, 1)
+    x_arr = np.loadtxt(
+        wrkdir_DNR + "Figures/sum21/sheet_txt/y{}/1200.txt".format(xcut)
+    )[:, 0]
+    val_mesh = np.array(
+        [
+            np.loadtxt(
+                wrkdir_DNR + "Figures/sum21/sheet_txt/y{}/{}.txt".format(xcut, fnr)
+            )[:, 1]
+            for fnr in fnr_range
+        ]
+    )
+
+    fig, ax = plt.subplots(1, 1)
+    ax.grid()
+    ax.set(
+        xlabel="X [Re]",
+        ylabel="Time [s]",
+        title="Y = {} Re".format(xcut),
+        xlim=(-10, 10),
+    )
+
+    ax.pcolormesh(x_arr, fnr_range, val_mesh, shading="nearest", cmap="seismic")
+
+    for fnr in fnr_range:
+        ffjs = np.loadtxt(
+            "/wrk/group/spacephysics/vlasiator/3D/EGI/visualizations/FFJ/dx_2e6_series/{}/ascii_rxpoints_tail_neighbourhood_1_extend_4_4_4_000{}.dat".format(
+                fnr, fnr
+            )
+        )
+        x, y, z = ffjs.T
+        x_plot = x[np.abs(y - xcut) < 0.2]
+        t_plot = np.ones_like(x_plot) * fnr
+        ax.plot(x_plot, t_plot, "^", color="black", markersize=1)
+
+    plt.tight_layout()
+    # fig.savefig(wrkdir_DNR + "Figures/sum21/tail_sheet_jplot_x{}.pdf".format(xcut))
+    fig.savefig(wrkdir_DNR + "Figures/sum21/tail_sheet_jplot_y{}.png".format(xcut))
     plt.close(fig)
 
     return None
