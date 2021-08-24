f9b8c18e516896e14f3e96af0295b7665ed2bcc0
JonasSuni <jonas.suni@helsinki.fi>
2021-08-24 10:40:03 +0300

    Test

--- pyJets/jet_21_scripts.py
+++ pyJets/jet_21_scripts.py
@@ -301,7 +301,7 @@ def ballooning_crit(B, P, beta):
 
     balloon = (2 + beta) / 4.0 * kappaP / (kappaC + 1e-27)
 
-    return (balloon, nnorm, kappaC)
+    return (balloon, n, kappaC)
 
 
 def plot_ballooning(tstep=1274, xcut=15):
@@ -424,6 +424,9 @@ def ext_plot_ballooning(ax, XmeshXY, YmeshXY, pass_maps):
     U = nnorm_arr[:, 1, :, 0]
     V = nnorm_arr[:, 1, :, 2]
     C = nnorm_arr[:, 1, :, 1]
+
+    U = U / np.linalg.norm(nnorm_arr[:, 1, :, 0::2], axis=-1)
+    V = V / np.linalg.norm(nnorm_arr[:, 1, :, 0::2], axis=-1)
 
     ax.contour(XmeshXY, YmeshXY, vx, 0, colors="black", linewidths=1.2)
     ax.contour(XmeshXY, YmeshXY, Bx, 0, colors="red", linewidths=0.8)
