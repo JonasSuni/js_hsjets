20f5f2dbd54b68ab83cac89e0c859d430c5b92d5
JonasSuni <jonas.suni@helsinki.fi>
2021-08-30 10:09:12 +0300

    Test

--- pyJets/jet_21_scripts.py
+++ pyJets/jet_21_scripts.py
@@ -513,8 +513,8 @@ def ext_plot_ballooning(ax, XmeshXY, YmeshXY, pass_maps):
     Jcb.ax.tick_params(labelsize=6)
     Jcb.set_label("J [nA/m$^2$]", size=6, loc="bottom")
 
-    ax.contour(XmeshXY, YmeshXY, vx, 0, colors="black", linewidths=0.8)
-    ax.contour(XmeshXY, YmeshXY, Bx, 0, colors="red", linewidths=0.8)
+    ax.contour(XmeshXY, YmeshXY, vx, 0, colors="black", linewidths=0.4)
+    ax.contour(XmeshXY, YmeshXY, Bx, 0, colors="red", linewidths=0.4)
 
     Balloon_im = ax.pcolormesh(
         XmeshXY,
