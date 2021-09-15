dc7e4f13f76ee425270d01159a831418bd7f13b8
JonasSuni <jonas.suni@helsinki.fi>
2021-09-15 12:26:20 +0300

    Added Carrington module

--- jstools.py
+++ jstools.py
@@ -1,8 +1,8 @@
-'''
+"""
 Imports all the modules from subfolders
-'''
+"""
 
-import sys,os
+import sys, os
 import socket, re
 
 # Input current folder's path
@@ -12,25 +12,31 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/" + "pyJets")
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/" + "pyLandau")
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/" + "pySlams")
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/" + "pyThesis")
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/" + "pyCarrington")
 
-#Import modules
+# Import modules
 
 try:
     import jets
 except ImportError as e:
-    print("Note: Did not import jets module: ",e)
+    print("Note: Did not import jets module: ", e)
 
 try:
     import landau
 except ImportError as e:
-    print("Note: Did not import landau module: ",e)
+    print("Note: Did not import landau module: ", e)
 
 try:
     import slams
 except ImportError as e:
-    print("Note: Did not import slams module: ",e)
+    print("Note: Did not import slams module: ", e)
 
 try:
     import thesis
 except ImportError as e:
-    print("Note: Did not import thesis module: ",e)
+    print("Note: Did not import thesis module: ", e)
+
+try:
+    import carrington
+except ImportError as e:
+    print("Note: Did not import carrington module: ", e)
