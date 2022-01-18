6f66cc129eeb6f6f0b8ab6e8d2279dbee5690741
JonasSuni <jonas.suni@helsinki.fi>
2022-01-18 13:34:10 +0200

    Test

--- pyJets/papu_2.py
+++ pyJets/papu_2.py
@@ -28,7 +28,13 @@ except:
 
 
 def sj_non_timeseries(runid):
+    """
+    Variables: t, n, v, Pdyn, B, Tperp, Tpar
+    Count: 7
+    """
 
+    # Solar wind parameters for the different runs
+    # n [m^-3], v [m/s], B [T], T [K]
     runid_list = ["ABA", "ABC", "AEA", "AEC"]
     sw_pars = [
         [1e6, 750e3, 5e-9, 0.5e6],
@@ -36,36 +42,44 @@ def sj_non_timeseries(runid):
         [1e6, 750e3, 10e-9, 0.5e6],
         [3.3e6, 600e3, 10e-9, 0.5e6],
     ]
-
     n_sw, v_sw, B_sw, T_sw = sw_pars[runid_list.index(runid)]
 
-    """
-    Variables: t, n, v, Pdyn, B, Tperp, Tpar
-    Count: 7
-    """
+    # Path to vlsv files for current run
     bulkpath = jx.find_bulkpath(runid)
 
+    # Get IDs of fcs-jets and non-fcs-jets
     sj_jet_ids, jet_ids, slams_ids = jh20.separate_jets_god(runid, False)
     non_sj_ids = jet_ids[np.in1d(jet_ids, sj_jet_ids) == False]
 
+    # Loop through fcs-jet IDs
     for sj_id in sj_jet_ids:
         print("FCS jets: {}".format(sj_id))
         out_arr = []
 
+        # Read jet position, time and filenumber at time of birth
         props = jio.PropReader(str(sj_id).zfill(5), runid, transient="jet")
         x0, y0 = (props.read("x_mean")[0], props.read("y_mean")[0])
         t0 = props.read("time")[0]
         fnr0 = int(t0 * 2)
+
+        # Initialise +-5s array of file numbers
         fnr_arr = np.arange(fnr0 - 10, fnr0 + 11)
+
+        # Get cellid of initial position
         cellid = pt.vlsvfile.VlsvReader(
             bulkpath + "bulk.{}.vlsv".format(str(fnr0).zfill(7))
         ).get_cellid([x0 * r_e, y0 * r_e, 0 * r_e])
 
+        # Loop through filenumbers in time series
         for fnr in fnr_arr:
+
+            # Try if file exists
             try:
                 vlsvobj = pt.vlsvfile.VlsvReader(
                     bulkpath + "bulk.{}.vlsv".format(str(fnr).zfill(7))
                 )
+
+                # Read time, n, v, Pdyn, B, Tperp, Tpar
                 t = float(fnr) / 2.0
                 n = vlsvobj.read_variable("rho", cellids=cellid)
                 v = vlsvobj.read_variable("v", cellids=cellid, operator="magnitude")
@@ -73,6 +87,8 @@ def sj_non_timeseries(runid):
                 B = vlsvobj.read_variable("B", cellids=cellid, operator="magnitude")
                 Tperp = vlsvobj.read_variable("TPerpendicular", cellids=cellid)
                 Tpar = vlsvobj.read_variable("TParallel", cellids=cellid)
+
+                # Append normalised properties to array
                 out_arr.append(
                     np.array(
                         [
@@ -87,36 +103,49 @@ def sj_non_timeseries(runid):
                     )
                 )
             except:
+                # If vlsv file doesn't exist, append nans
                 out_arr.append(
                     np.array([np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan])
                 )
 
         out_arr = np.array(out_arr)
 
+        # Save time series as txt file
         np.savetxt(
             wrkdir_DNR + "papu22/fcs_jets/{}/timeseries_{}.txt".format(runid, sj_id),
             out_arr,
             fmt="%.7f",
         )
 
+    # Loop through non-fcs-jet IDs
     for non_id in non_sj_ids:
         print("Non-FCS jets: {}".format(non_id))
         out_arr = []
 
+        # Read jet position, time and filenumber at time of birth
         props = jio.PropReader(str(non_id).zfill(5), runid, transient="jet")
         x0, y0 = (props.read("x_mean")[0], props.read("y_mean")[0])
         t0 = props.read("time")[0]
         fnr0 = int(t0 * 2)
+
+        # Initialise +-5s array of file numbers
         fnr_arr = np.arange(fnr0 - 10, fnr0 + 11)
+
+        # Get cellid of initial position
         cellid = pt.vlsvfile.VlsvReader(
             bulkpath + "bulk.{}.vlsv".format(str(fnr0).zfill(7))
         ).get_cellid([x0 * r_e, y0 * r_e, 0 * r_e])
 
+        # Loop through filenumbers in time series
         for fnr in fnr_arr:
+
+            # Try if file exists
             try:
                 vlsvobj = pt.vlsvfile.VlsvReader(
                     bulkpath + "bulk.{}.vlsv".format(str(fnr).zfill(7))
                 )
+
+                # Read time, n, v, Pdyn, B, Tperp, Tpar
                 t = float(fnr) / 2.0
                 n = vlsvobj.read_variable("rho", cellids=cellid)
                 v = vlsvobj.read_variable("v", cellids=cellid, operator="magnitude")
@@ -124,6 +153,8 @@ def sj_non_timeseries(runid):
                 B = vlsvobj.read_variable("B", cellids=cellid, operator="magnitude")
                 Tperp = vlsvobj.read_variable("TPerpendicular", cellids=cellid)
                 Tpar = vlsvobj.read_variable("TParallel", cellids=cellid)
+
+                # Append normalised properties to array
                 out_arr.append(
                     np.array(
                         [
@@ -138,14 +169,102 @@ def sj_non_timeseries(runid):
                     )
                 )
             except:
+                # If vlsv file doesn't exist, append nans
                 out_arr.append(
                     np.array([np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan])
                 )
 
         out_arr = np.array(out_arr)
 
+        # Save time series as txt file
         np.savetxt(
             wrkdir_DNR + "papu22/non_jets/{}/timeseries_{}.txt".format(runid, non_id),
             out_arr,
             fmt="%.7f",
         )
+
+
+def SEA_plots():
+    """
+    Superposed epoch analysis of fcs-jet vs. non-fcs-jet start location properties
+    """
+
+    t_arr = np.arange(-5.0, 5.05, 0.5)
+
+    fcs_jet_count = 0
+    non_jet_count = 0
+
+    fcs_jet_avg = np.zeros((6, 21), dtype=float)
+    non_jet_avg = np.zeros((6, 21), dtype=float)
+
+    fig, ax_list = plt.subplots(6, 1, sharex=True)
+
+    for ax in ax_list:
+        ax.grid()
+
+    ax_list[0].set_ylabel("$\Delta n~[n_\mathrm{sw}]$")
+    ax_list[1].set_ylabel("$\Delta |v|~[v_\mathrm{sw}]$")
+    ax_list[2].set_ylabel("$\Delta P_\mathrm{dyn}~[P_\mathrm{dyn,sw}]$")
+    ax_list[3].set_ylabel("$\Delta |B|~[B_\mathrm{IMF}]$")
+    ax_list[4].set_ylabel("$\Delta T_\mathrm{perp}~[T_\mathrm{sw}]$")
+    ax_list[5].set_ylabel("$\Delta T_\mathrm{par}~[T_\mathrm{sw}]$")
+    ax_list[-1].set_xlabel("$\Delta t~[\mathrm{s}]$")
+
+    for runid in ["ABA", "ABC", "AEA", "AEC"]:
+        for n1 in range(4000):
+            try:
+                data = np.loadtxt(
+                    wrkdir_DNR
+                    + "papu22/fcs_jets/{}/timeseries_{}.txt".format(runid, n1)
+                ).T
+                fcs_jet_count += 1
+
+                for n2 in range(6):
+                    ax_list[n2].plot(
+                        t_arr, data[n2] - data[n2][10], color="darkgray", zorder=1
+                    )
+                    fcs_jet_avg[n2] += data[n2] - data[n2][10]
+            except:
+                pass
+
+            try:
+                data = np.loadtxt(
+                    wrkdir_DNR
+                    + "papu22/non_jets/{}/timeseries_{}.txt".format(runid, n1)
+                ).T
+                non_jet_count += 1
+
+                for n2 in range(6):
+                    ax_list[n2].plot(
+                        t_arr, data[n2] - data[n2][10], color="darkgray", zorder=1
+                    )
+                    non_jet_avg[n2] += data[n2] - data[n2][10]
+            except:
+                pass
+
+    fcs_jet_avg /= fcs_jet_count
+    non_jet_avg /= non_jet_count
+
+    for n2 in range(6):
+        ax_list[n2].plot(
+            t_arr,
+            fcs_jet_avg[n2],
+            color=jx.CB_color_cycle[0],
+            label="FCS-jets",
+            zorder=2,
+        )
+        ax_list[n2].plot(
+            t_arr,
+            non_jet_avg[n2],
+            color=jx.CB_color_cycle[1],
+            label="non-FCS-jets",
+            zorder=2,
+        )
+
+    ax_list[0].legend()
+
+    plt.tight_layout()
+
+    fig.savefig(wrkdir_DNR + "papu22/Figures/SEA_plot.pdf")
+    fig.savefig(wrkdir_DNR + "papu22/Figures/SEA_plot.png")
+    plt.close(fig)
