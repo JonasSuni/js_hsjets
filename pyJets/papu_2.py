e23739037972c0aa1e7311014aee578a732e3853
JonasSuni <jonas.suni@helsinki.fi>
2022-12-02 17:00:54 +0200

    Fix

--- pyJets/papu_2.py
+++ pyJets/papu_2.py
@@ -4376,12 +4376,8 @@ def kinds_pca():
                 # data_arr.append(ts_data.flatten())
                 # classes_arr[idx].append(ts_data.flatten())
 
-                data_arr.append(
-                    ts_data[[0, 1, 2, 6, 7, 10, 11], [10, 20, 30]].flatten()
-                )
-                classes_arr[idx].append(
-                    ts_data[[0, 1, 2, 6, 7, 10, 11], [10, 20, 30]].flatten()
-                )
+                data_arr.append(ts_data[[0, 1, 2, 6, 7, 10, 11], 20].flatten())
+                classes_arr[idx].append(ts_data[[0, 1, 2, 6, 7, 10, 11], 20].flatten())
 
                 # data_arr.append(ts_data[[0, 1, 2, 5, 6, 7, 11], :].flatten())
                 # data_arr.append(ts_data[[0, 1, 2, 3, 5, 6, 7, 8, 10, 11], 20])
