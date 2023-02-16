5ecaa22cbc66c702b2817722ed9a05d4b129d7a9
JonasSuni <jonas.suni@helsinki.fi>
2023-02-16 11:36:18 +0200

    Test

--- pyJets/papu_2.py
+++ pyJets/papu_2.py
@@ -3762,7 +3762,7 @@ def jet_var_plotter(runid, var):
         "vBackstream",
     ]
     vscale = [1e9, 1e-6, 1e-6, 1e9, 1e-3, 1e-3][vars_list.index(var)]
-    vmax = [10, 10, 10, 10, 250, 250][vars_list.index(var)]
+    vmax = [1.5, 10, 10, 10, 250, 250][vars_list.index(var)]
 
     runids = ["ABA", "ABC", "AEA", "AEC"]
     runids_pub = ["HM30", "HM05", "LM30", "LM05"]
@@ -3826,7 +3826,8 @@ def jet_var_plotter(runid, var):
             # ),
             var=var,
             vmin=0,
-            vmax=1,
+            # vmax=1,
+            vmax=vmax,
             vscale=vscale,
             # cbtitle="$P_{dyn}$ [nPa]",
             # cbtitle="",
