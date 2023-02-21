d5df5995fb4ecf7510bc93da8505fecf1d6b04be
JonasSuni <jonas.suni@helsinki.fi>
2023-02-21 13:57:10 +0200

    Test

--- pyJets/papu_2.py
+++ pyJets/papu_2.py
@@ -4494,8 +4494,8 @@ def SEA_timeseries_comp():
         "$B~[B_\mathrm{IMF}]$",
         "$T~[T_\mathrm{sw}]$",
     ]
-    vmins = [1.3, -0.8, 0.3, -3.75, 0]
-    vmaxs = [5, 0.9, 1.7, 3.75, 35]
+    vmins = [0.8, -1.0, 0.1, -3.8, -5]
+    vmaxs = [5, 1.0, 2.0, 3.8, 40]
     plot_index = [0, 1, 1, 1, 1, 2, 3, 3, 3, 3, 4, 4]
     plot_colors = [
         "k",
@@ -4668,7 +4668,7 @@ def SEA_timeseries_comp():
         if idx2 == 0:
             ax.set_ylabel("$T_\perp/T_\parallel$", fontsize=40, labelpad=10)
         # ax.axvline(0, linestyle="dashed")
-        ax.set_ylim(0.5, 2.4)
+        ax.set_ylim(0.4, 2.6)
         ax.annotate(
             annot[idx2][-1], (0.05, 0.85), xycoords="axes fraction", fontsize=32
         )
