8a3402441de5ae620ec8383f817db62b604bd05c
JonasSuni <jonas.suni@helsinki.fi>
2023-02-21 10:38:00 +0200

    Fix

--- pyJets/papu_2.py
+++ pyJets/papu_2.py
@@ -4661,7 +4661,7 @@ def SEA_timeseries_comp():
         if idx2 == 0:
             ax.set_ylabel("$T_\perp/T_\parallel$", fontsize=40, labelpad=10)
         # ax.axvline(0, linestyle="dashed")
-        ax.set_ylim(0, 2.4)
+        ax.set_ylim(0.5, 2.4)
         # ax.annotate(
         #     annot[idx2][idx], (0.05, 0.85), xycoords="axes fraction", fontsize=32
         # )
