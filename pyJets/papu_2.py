e7b0e93774c835fc1737ebcc6594047f86d8da0b
JonasSuni <jonas.suni@helsinki.fi>
2023-02-21 14:00:21 +0200

    Test

--- pyJets/papu_2.py
+++ pyJets/papu_2.py
@@ -4607,7 +4607,7 @@ def SEA_timeseries_comp():
             ax.boxplot(
                 epoch_mag_arr[idx2, idx, :, : counters[idx2]].T,
                 # positions=[0],
-                positions=np.arange(-6.5, 7.5, 6.5),
+                positions=np.arange(-6.49, 7.51, 6.51),
                 manage_ticks=False,
                 widths=1.0,
                 sym="",
@@ -4648,7 +4648,7 @@ def SEA_timeseries_comp():
         ax.boxplot(
             T_ani[idx2, :, : counters[idx2]].T,
             # positions=[0],
-            positions=np.arange(-6.5, 7.5, 6.5),
+            positions=np.arange(-6.49, 7.51, 6.51),
             manage_ticks=False,
             widths=1.0,
             sym="",
@@ -4668,7 +4668,7 @@ def SEA_timeseries_comp():
         if idx2 == 0:
             ax.set_ylabel("$T_\perp/T_\parallel$", fontsize=40, labelpad=10)
         # ax.axvline(0, linestyle="dashed")
-        ax.set_ylim(0.4, 2.6)
+        ax.set_ylim(0.4, 2.7)
         ax.annotate(
             annot[idx2][-1], (0.05, 0.85), xycoords="axes fraction", fontsize=32
         )
