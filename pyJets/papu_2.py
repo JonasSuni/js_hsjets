00f5a6bcefa63ba3b2359e3c0d6003db8dcbc936
JonasSuni <jonas.suni@helsinki.fi>
2023-02-22 11:31:54 +0200

    Test

--- pyJets/papu_2.py
+++ pyJets/papu_2.py
@@ -4628,7 +4628,7 @@ def SEA_timeseries_comp():
                 manage_ticks=False,
                 widths=1.0,
                 sym="",
-                whis=(0,100),
+                whis=(0, 100),
                 notch=False,
                 boxprops=dict(color=plot_colors[idx]),
                 capprops=dict(color=plot_colors[idx]),
@@ -4649,7 +4649,7 @@ def SEA_timeseries_comp():
             if idx2 == 0:
                 ax.set_ylabel(ylabels[idx], fontsize=40, labelpad=10)
             # ax.axvline(0, linestyle="dashed")
-            ax.set_ylim(vmins[idx], vmaxs[idx])
+            # ax.set_ylim(vmins[idx], vmaxs[idx])
             ax.annotate(
                 annot[idx2][idx], (0.05, 0.85), xycoords="axes fraction", fontsize=32
             )
@@ -4669,7 +4669,7 @@ def SEA_timeseries_comp():
             manage_ticks=False,
             widths=1.0,
             sym="",
-            whis=(0,100),
+            whis=(0, 100),
             notch=False,
             boxprops=dict(color="k"),
             capprops=dict(color="k"),
@@ -4685,7 +4685,7 @@ def SEA_timeseries_comp():
         if idx2 == 0:
             ax.set_ylabel("$T_\perp/T_\parallel$", fontsize=40, labelpad=10)
         # ax.axvline(0, linestyle="dashed")
-        ax.set_ylim(0.4, 2.7)
+        # ax.set_ylim(0.4, 2.7)
         ax.annotate(
             annot[idx2][-1], (0.05, 0.85), xycoords="axes fraction", fontsize=32
         )
