b86e5f9e4a1f5cf9b140e9cc62066a53cd06b816
JonasSuni <jonas.suni@helsinki.fi>
2023-02-23 08:13:54 +0200

    Fix

--- pyJets/papu_2.py
+++ pyJets/papu_2.py
@@ -4628,8 +4628,8 @@ def SEA_timeseries_comp():
         "$B~[B_\mathrm{IMF}]$",
         "$T~[T_\mathrm{sw}]$",
     ]
-    vmins = [0.8, -1.0, 0.1, -3.8, -5]
-    vmaxs = [5, 1.0, 2.0, 3.8, 40]
+    vmins = [0.5, -1.1, 0, -4.2, 0]
+    vmaxs = [6, 1.1, 2.5, 4.2, 50]
     plot_index = [0, 1, 1, 1, 2, 3, 3, 3, 4, 4]
     offsets = np.array([0, -0.2, 0.2, 0, 0, -0.2, 0.2, 0, 0, -0.2]) - 0.1
     plot_colors = [
@@ -4801,7 +4801,7 @@ def SEA_timeseries_comp():
         if idx2 == 0:
             ax.set_ylabel("$T_\perp/T_\parallel$", fontsize=40, labelpad=10)
         # ax.axvline(0, linestyle="dashed")
-        ax.set_ylim(0.4, 2.7)
+        ax.set_ylim(0.2, 3)
         ax.annotate(
             annot[idx2][-1], (0.05, 0.85), xycoords="axes fraction", fontsize=32
         )
