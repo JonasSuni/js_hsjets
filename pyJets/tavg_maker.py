06f276b6855f7a814925e02c9f45419c5653fabd
JonasSuni <jonas.suni@helsinki.fi>
2023-04-03 15:59:17 +0300

    Test

--- pyJets/tavg_maker.py
+++ pyJets/tavg_maker.py
@@ -106,7 +106,7 @@ def tavg_maker_2023(runid, fnr, parallel=True):
             if i == fnr:
                 continue
             res = pool.apply_async(get_pdyn(i))
-            pdyn_avg[:] += res
+            pdyn_avg[:] += res.get(timeout=1)[:]
         pool.close()
         pool.join()
 
