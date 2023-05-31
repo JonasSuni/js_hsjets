185a51bc0927d5bd73f8bb4f951e4c1aa6c90d9c
JonasSuni <jonas.suni@helsinki.fi>
2023-05-31 11:07:37 +0300

    Test

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -2206,7 +2206,9 @@ def multi_VSC_timeseries(runid="AGF", time0=480, x=[8], y=[7], pm=60, delta=Fals
     if delta:
         for idx, lab in enumerate(ts_v_labels):
             lnew = lab.split("~")[0]
-            ts_v_labels[idx] = "$\\delta$" + lnew + "/<{}>$".format(lnew[1:])
+            ts_v_labels[idx] = (
+                "$\\delta$" + lnew + "/\\langle{}\\rangle$".format(lnew[1:])
+            )
     nrows = len(ts_v_labels)
 
     ts_arr = np.zeros((nvsc, nrows, nt), dtype=float)
