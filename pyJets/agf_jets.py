2096c34a8db92e162da49cd2b6e6e8bd91e3f9fc
JonasSuni <jonas.suni@helsinki.fi>
2023-06-08 11:04:31 +0300

    Fix

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -2344,35 +2344,35 @@ def multi_VSC_timeseries(runid="AGF", time0=480, x=[8], y=[7], pm=60, delta=Fals
                 )
 
     for idx2 in range(nvsc):
-        ax_list[-3].grid()
-        ax_list[-3].set_xlim(t_arr[0], t_arr[-1])
-        ax_list[-3].plot(
+        ax_list[nrows].grid()
+        ax_list[nrows].set_xlim(t_arr[0], t_arr[-1])
+        ax_list[nrows].plot(
             t_arr,
             (ts_arr[idx2, 0, :] - np.mean(ts_arr[idx2, 0, :]))
             * (ts_arr[idx2, 4, :] - np.mean(ts_arr[idx2, 4, :])),
             color=CB_color_cycle[idx2],
         )
-        ax_list[-3].set_ylabel("$\\delta n \\times \\delta v$")
+        ax_list[nrows].set_ylabel("$\\delta n \\times \\delta v$")
 
-        ax_list[-2].grid()
-        ax_list[-2].set_xlim(t_arr[0], t_arr[-1])
-        ax_list[-2].plot(
+        ax_list[nrows + 1].grid()
+        ax_list[nrows + 1].set_xlim(t_arr[0], t_arr[-1])
+        ax_list[nrows + 1].plot(
             t_arr,
             (ts_arr[idx2, 0, :] - np.mean(ts_arr[idx2, 0, :]))
             * (ts_arr[idx2, 9, :] - np.mean(ts_arr[idx2, 9, :])),
             color=CB_color_cycle[idx2],
         )
-        ax_list[-2].set_ylabel("$\\delta n \\times \\delta B$")
+        ax_list[nrows + 1].set_ylabel("$\\delta n \\times \\delta B$")
 
-        ax_list[-1].grid()
-        ax_list[-1].set_xlim(t_arr[0], t_arr[-1])
-        ax_list[-1].plot(
+        ax_list[nrows + 2].grid()
+        ax_list[nrows + 2].set_xlim(t_arr[0], t_arr[-1])
+        ax_list[nrows + 2].plot(
             t_arr,
             (ts_arr[idx2, 9, :] - np.mean(ts_arr[idx2, 9, :]))
             * (ts_arr[idx2, 4, :] - np.mean(ts_arr[idx2, 4, :])),
             color=CB_color_cycle[idx2],
         )
-        ax_list[-1].set_ylabel("$\\delta B \\times \\delta v$")
+        ax_list[nrows + 2].set_ylabel("$\\delta B \\times \\delta v$")
 
     ax_list[0].legend(loc="lower left")
     ax_list[0].set_title("VSC: {}".format(coords[:, :2] / r_e))
