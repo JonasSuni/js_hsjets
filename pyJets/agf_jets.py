3bd104f36954bba2b76c18b7602bc2e5fadee93f
JonasSuni <jonas.suni@helsinki.fi>
2023-12-04 11:40:01 +0200

    Test

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -2375,6 +2375,7 @@ def VSC_timeseries(runid, x0, y0, t0, t1, pdavg=True):
     ]
 
     run_norm = norm[0]
+    sos = butter(10, 0.01, "low", fs=2, output="sos")
 
     t_arr = np.arange(t0, t1 + 0.1, 0.5)
     fnr0 = int(t0 * 2)
@@ -2409,6 +2410,9 @@ def VSC_timeseries(runid, x0, y0, t0, t1, pdavg=True):
         except:
             print("Something went wrong!")
             data_arr[:, idx] = np.nan
+
+    for idx in range(len(var_list)):
+        data_arr[idx, :] = sosfilt(sos, data_arr[idx, :])
 
     fig, ax_list = plt.subplots(
         len(ylabels), 1, sharex=True, figsize=(6, 8), constrained_layout=True
