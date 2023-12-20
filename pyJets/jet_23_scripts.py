b6f4bc6bf8b8900efae176c1c75d9f52bcf57e30
JonasSuni <jonas.suni@helsinki.fi>
2023-12-20 14:49:29 +0200

    Test

--- pyJets/jet_23_scripts.py
+++ pyJets/jet_23_scripts.py
@@ -689,25 +689,25 @@ def ext_jet_slim(ax, XmeshXY, YmeshXY, pass_maps):
 
     CB_color_cycle = jx.CB_color_cycle
 
-    start_points = np.array(
-        [np.ones(20) * x0 + 0.5, np.linspace(y0 - 0.9, y0 + 0.9, 20)]
-    ).T
+    # start_points = np.array(
+    #     [np.ones(20) * x0 + 0.5, np.linspace(y0 - 0.9, y0 + 0.9, 20)]
+    # ).T
     # start_points = np.array([np.linspace(x0 - 0.9, x0 + 0.9, 10), np.ones(10) * y0]).T
 
-    if Blines_g:
-        stream = ax.streamplot(
-            XmeshXY,
-            YmeshXY,
-            B[:, :, 0],
-            B[:, :, 1],
-            # arrowstyle="-",
-            # broken_streamlines=False,
-            color="k",
-            linewidth=0.6,
-            # minlength=4,
-            density=35,
-            start_points=start_points,
-        )
+    # if Blines_g:
+    #     stream = ax.streamplot(
+    #         XmeshXY,
+    #         YmeshXY,
+    #         B[:, :, 0],
+    #         B[:, :, 1],
+    #         # arrowstyle="-",
+    #         # broken_streamlines=False,
+    #         color="k",
+    #         linewidth=0.6,
+    #         # minlength=4,
+    #         density=35,
+    #         start_points=start_points,
+    #     )
 
     jet_cont = ax.contour(
         XmeshXY,
