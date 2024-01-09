a8db23b3ea5595f3a80d781b75e85724a93ebb40
JonasSuni <jonas.suni@helsinki.fi>
2024-01-09 13:39:38 +0200

    Test

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -4683,8 +4683,10 @@ def run_comp_plotter(
                     "proton/vg_Pdynx",
                     "proton/vg_beta_star",
                 ],
-                fluxdir=bulkpath + "../fluxfunction",
-                fluxfile=fname,
+                fluxdir="/wrk-vakka/group/spacephysics/vlasiator/2D/{}/fluxfunction".format(
+                    runids[idx]
+                ),
+                fluxfile=fname + ".bin",
                 fluxlines=10,
             )
 
