1c377315e54a47d748b9c18e1be8203c9cec82e9
JonasSuni <jonas.suni@helsinki.fi>
2024-01-25 10:15:30 +0200

    Fix?

--- pyJets/jet_aux.py
+++ pyJets/jet_aux.py
@@ -701,12 +701,11 @@ def get_neighs_asym(runid, cells, neighborhood_reach=[-1, 1, -1, 1, 0, 0]):
 
 
 def find_bulkpath(runid):
-    runid_list = ["ABA", "ABC", "AEA", "AEC", "BFD", "AGF", "AIA", "AIB", "static_IB_B"]
+    runid_list = ["ABA", "ABC", "AEA", "AEC", "BFD", "AGF", "AIA", "AIB"]
     path_list = [
         "bulk/",
         "bulk/",
         "round_3_boundary_sw/",
-        "bulk/",
         "bulk/",
         "bulk/",
         "bulk/",
