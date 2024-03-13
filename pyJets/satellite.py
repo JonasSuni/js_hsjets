418057e43e29db1038c5d1767a85abccf554866d
JonasSuni <jonas.suni@helsinki.fi>
2024-03-13 11:27:01 +0200

    Fix

--- pyJets/satellite.py
+++ pyJets/satellite.py
@@ -243,7 +243,7 @@ def plot_thd_mms1_c4(t0, t1):
         None,
         None,
     ]
-    ylims = [(-10, 10), (-500, 500), (0, 50), (0, 10)]
+    ylims = [(-50, 50), (-500, 500), (0, 50), (0, 10)]
 
     fig, ax_list = plt.subplots(4, 3, figsize=(18, 12), sharey="row")
 
