2da5731116d90f47da6ed6993c6a9154744add75
JonasSuni <jonas.suni@helsinki.fi>
2024-03-25 10:22:18 +0200

    Test

--- pyJets/satellite.py
+++ pyJets/satellite.py
@@ -117,6 +117,10 @@ def MVA(data):
     eigenval, eigenvec = np.linalg.eig(M)
     eigenvec = eigenvec.T
 
+    for idx in range(3):
+        if eigenvec[idx][0] > 0:
+            eigenvec[idx] *= -1
+
     print("\n")
     print("Eigenvalues: ", np.sort(eigenval))
     print("Eigenvectors: ", eigenvec[np.argsort(eigenval)])
