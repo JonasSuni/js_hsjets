6453cdccb3f09342299e6b447f5d5162a7db3828
JonasSuni <jonas.suni@helsinki.fi>
2024-04-03 11:50:25 +0300

    Fix

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -5666,9 +5666,9 @@ def MVA(data):
     eigenval, eigenvec = np.linalg.eig(M)
     eigenvec = eigenvec.T
 
-    for idx in range(3):
-        if eigenvec[0][0] > 0:
-            eigenvec[idx] *= -1
+    # for idx in range(3):
+    #     if eigenvec[0][0] > 0:
+    #         eigenvec[idx] *= -1
 
     print("\n")
     print("Eigenvalues: ", np.sort(eigenval))
