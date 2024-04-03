dfbd76c5abde68139904ba62eace8b68e2fd75a7
JonasSuni <jonas.suni@helsinki.fi>
2024-04-03 11:48:47 +0300

    SASDDSSD

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -2467,9 +2467,9 @@ def VSC_timeseries(
         print(Bdata.shape)
         print("Minimum Variance direction: {}".format(eigenvecs[0]))
         for idx in range(3):
-            data_arr[idx + 6, :] = np.dot(Bdata.T, eigenvecs[idx,:])
-            data_arr[idx + 1, :] = np.dot(vdata.T, eigenvecs[idx,:])
-            data_arr[idx + 10, :] = np.dot(Edata.T, eigenvecs[idx,:])
+            data_arr[idx + 6, :] = np.dot(Bdata.T, eigenvecs[idx, :])
+            data_arr[idx + 1, :] = np.dot(vdata.T, eigenvecs[idx, :])
+            data_arr[idx + 10, :] = np.dot(Edata.T, eigenvecs[idx, :])
         print(np.linalg.norm(data_arr[6:9, :].T[0]))
         plot_labels[1:4] = ["$v_N$", "$v_M$", "$v_L$"]
         plot_labels[6:9] = ["$B_N$", "$B_M$", "$B_L$"]
