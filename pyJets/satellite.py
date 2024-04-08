5831014c31151295c522cb4873121a2971665366
JonasSuni <jonas.suni@helsinki.fi>
2024-04-08 14:00:05 +0300

    Test

--- pyJets/satellite.py
+++ pyJets/satellite.py
@@ -1608,6 +1608,10 @@ def diag_sc_mva(sc, probe, t0, t1, dt=1, grain=1, datarate="srvy", cutoff=0.9):
     )
     plt.close(fig)
 
+    i, j = np.where(diag_data.T == np.max(diag_data.T))[0]
+
+    return diag_vec_data[i, j, :]
+
 
 def diag_thd_mms1_c4(t0, t1, dt=1, sc_order=[0, 1, 2], grain=1):
 
