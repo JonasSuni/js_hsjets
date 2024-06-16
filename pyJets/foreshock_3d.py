20c22f3797e2c27dabe2dd53c913b483294d7cfb
JonasSuni <jonas.suni@helsinki.fi>
2024-06-16 08:47:29 +0300

    Test

--- pyJets/foreshock_3d.py
+++ pyJets/foreshock_3d.py
@@ -111,7 +111,7 @@ def ipshock_1d_compare(fnr=36, resols=[250, 300, 500, 1000, 2000, 4000, 8000]):
     scales = [1e-6, 1e-6, 1e-3, 1e-3]
     miny = [None, 10**-4, 0, 0]
     maxy = [5, 5, 1000, 1000]
-    op = ["pass", "pass", "magnitude", "magnitude"]
+    op = ["pass", "pass", "x", "x"]
     yscales = ["log", "log", "linear", "linear"]
 
     fig, ax_list = plt.subplots(
