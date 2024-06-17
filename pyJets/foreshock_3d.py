cdb6f50d588380073ae4a4b5993ead399df351b3
JonasSuni <jonas.suni@helsinki.fi>
2024-06-17 11:11:18 +0300

    Fix

--- pyJets/foreshock_3d.py
+++ pyJets/foreshock_3d.py
@@ -121,7 +121,7 @@ def ipshock_1d_vdf(x0=20):
             setThreshold=1e-18,
             box=[-6e6, 6e6, -6e6, 6e6],
             fmax=1e-5,
-            slicethick=0,
+            slicethick=1,
         )
         ax.set_title("dx = {} km".format(r))
     fig.suptitle("X = {} RE".format(x0))
