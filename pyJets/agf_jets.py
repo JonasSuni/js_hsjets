9a2f43a5b93136e4f37d659a3318a232d61eacdf
JonasSuni <jonas.suni@helsinki.fi>
2024-08-07 11:31:58 +0300

    Fix?

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -4697,7 +4697,7 @@ def pos_vdf_plotter(runid, x, y, t0, t1, skip=False):
     xg = []
     yg = []
 
-    global runid_g, sj_ids_g, non_ids_g, filenr_g, Blines_g, x0, y0, plaschke_g, drawBy0, linsg, draw_qperp,leg_g
+    global runid_g, sj_ids_g, non_ids_g, filenr_g, Blines_g, x0, y0, plaschke_g, drawBy0, linsg, draw_qperp, leg_g
     drawBy0 = False
     draw_qperp = False
     plaschke_g = False
