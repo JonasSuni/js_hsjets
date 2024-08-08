e88cf117676250986dae7eedbe0e433f746c7377
JonasSuni <jonas.suni@helsinki.fi>
2024-08-08 20:41:02 +0300

    Weird

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -4286,6 +4286,7 @@ def pos_vdf_1d_spectrogram(
     parperp=False,
     logcb=False,
     rotatetob=False,
+    filtsize=60,
 ):
     runids = ["AGF", "AIA", "AIB", "AIC"]
     bulkpath = find_bulkpath(runid)
@@ -4364,7 +4365,7 @@ def pos_vdf_1d_spectrogram(
         b = b / np.linalg.norm(b)
         b_arr[idx] = b
 
-    b_arr = uniform_filter1d(b_arr, size=10)
+    b_arr = uniform_filter1d(b_arr, size=filtsize)
 
     for idx, t in enumerate(np.arange(t0, t1 + 0.1, 0.5)):
         fnr = int(t * 2)
