6467187f16cdf925a36d66a2f5d9979df4bf4c4f
JonasSuni <jonas.suni@helsinki.fi>
2024-08-13 16:23:41 +0300

    Fix

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -2494,7 +2494,7 @@ def VSC_cut_through(
         data_arr[idx2 + 3, idx] = pos_mag_tension(vlsvobj, x_arr[idx], y_arr[idx]) * 1e9
 
     fig, ax_list = plt.subplots(
-        len(ylabels)+1, 1, sharex=True, figsize=(6, 8), constrained_layout=True
+        len(ylabels) + 1, 1, sharex=True, figsize=(6, 8), constrained_layout=True
     )
     ax_list[0].set_title(
         "Run: {}, $(x,y)_0$: {}, $(x,y)_1$: {}".format(runid, (x0, y0), (x1, y1))
@@ -2518,18 +2518,25 @@ def VSC_cut_through(
         if draw_legend[idx]:
             ax.legend(loc="center left", bbox_to_anchor=(1.01, 0.5))
     ylabels.append("$n_F$ [nPa/m]")
-    ax_list[-1].plot(n_arr,data_arr[-3],color=CB_color_cycle[0],label="$\\nabla p$")
-    ax_list[-1].plot(n_arr,data_arr[-2],color=CB_color_cycle[1],label="$\\nabla (B^2/2\\mu_0)$")
-    ax_list[-1].plot(n_arr,data_arr[-1],color=CB_color_cycle[1],label="$(B\\cdot\\nabla B)/\\mu_0$")
+    ax_list[-1].plot(n_arr, data_arr[-3], color=CB_color_cycle[0], label="$\\nabla p$")
+    ax_list[-1].plot(
+        n_arr, data_arr[-2], color=CB_color_cycle[1], label="$\\nabla (B^2/2\\mu_0)$"
+    )
+    ax_list[-1].plot(
+        n_arr,
+        data_arr[-1],
+        color=CB_color_cycle[2],
+        label="$(B\\cdot\\nabla B)/\\mu_0$",
+    )
     ax_list[-1].legend(loc="center left", bbox_to_anchor=(1.01, 0.5))
     for vline in vlines:
-            ax_list[-1].axvline(vline, linestyle="dashed", linewidth=0.6)
+        ax_list[-1].axvline(vline, linestyle="dashed", linewidth=0.6)
     ax_list[-1].set_xlim(n_arr[0], n_arr[-1])
     ax_list[-1].set_xlabel("Point along cut")
     for idx, ax in enumerate(ax_list):
         ax.grid()
         ax.set_ylabel(ylabels[idx])
-        #ax.axvline(t0, linestyle="dashed")
+        # ax.axvline(t0, linestyle="dashed")
     # plt.tight_layout()
     figdir = wrkdir_DNR + "Figs/cuts/"
     txtdir = wrkdir_DNR + "txts/cuts/"
