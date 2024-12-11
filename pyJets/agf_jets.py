437f097573a5bbda2ceca8bade1f654f5c53e086
JonasSuni <jonas.suni@helsinki.fi>
2024-12-11 10:40:44 +0200

    Refactor plot_jet_formation_postime to use list appending instead of np.append for improved performance

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -6295,14 +6295,14 @@ def plot_jet_formation_postime(
     s=1,
 ):
 
-    # y_values = []
-    # t_values = []
-    # maxsize_values = []
+    y_values = []
+    t_values = []
+    maxsize_values = []
     # duration_values = []
 
-    y_values = np.array([])
-    t_values = np.array([])
-    maxsize_values = np.array([])
+    # y_values = np.array([])
+    # t_values = np.array([])
+    # maxsize_values = np.array([])
 
     for n1 in range(6000):
         try:
@@ -6347,9 +6347,12 @@ def plot_jet_formation_postime(
         # maxsize_values.append(maxsize)
         # duration_values.append(duration)
 
-        y_values = np.append(y_values, ymean)
-        t_values = np.append(t_values, t)
-        maxsize_values = np.append(maxsize_values, props.read("Nr_cells"))
+        # y_values = np.append(y_values, ymean)
+        # t_values = np.append(t_values, t)
+        # maxsize_values = np.append(maxsize_values, props.read("Nr_cells"))
+        y_values.append(ymean)
+        t_values.append(t)
+        maxsize_values.append(props.read("Nr_cells"))
 
     fig, ax = plt.subplots(1, 1, figsize=(12, 8), constrained_layout=True)
     ax.grid(zorder=2.5)
@@ -6367,17 +6370,34 @@ def plot_jet_formation_postime(
     #     edgecolors="k",
     #     zorder=4.5,
     # )
-    ax.scatter(
-        t_values,
-        y_values,
-        c=np.log(maxsize_values),
-        cmap=cmap,
-        zorder=4.5,
-        marker="o",
-        edgecolors="none",
-        # alpha=0.5,
-        s=s,
-    )
+    # ax.scatter(
+    #     t_values,
+    #     y_values,
+    #     c=np.log(maxsize_values),
+    #     cmap=cmap,
+    #     zorder=4.5,
+    #     marker="o",
+    #     edgecolors="none",
+    #     # alpha=0.5,
+    #     s=s,
+    # )
+    for idx in range(len(y_values)):
+        ax.scatter(
+            t_values[idx],
+            y_values[idx],
+            c=np.log(maxsize_values[idx]),
+            cmap=cmap,
+            zorder=5.5,
+            marker="o",
+            edgecolors="none",
+            s=s,
+        )
+        ax.plot(
+            t_values[idx],
+            y_values[idx],
+            color="k",
+            linewidth=0.1,
+        )
 
     ax.add_patch(
         mpatches.Rectangle(
