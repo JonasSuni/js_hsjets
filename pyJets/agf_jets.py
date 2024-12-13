413d1a11b87613afdf269d65a1e6d8da6bc4edea
JonasSuni <jonas.suni@helsinki.fi>
2024-12-13 15:21:31 +0200

    Update zorder values and modify hatch pattern in plot_jet_formation_postime function

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -6341,19 +6341,19 @@ def plot_jet_formation_postime(
             y_values[idx],
             color="k",
             linewidth=0.2,
-            zorder=4.5,
+            zorder=5.5,
         )
         ax.fill_between(
             t_values[idx],
             ymin_values[idx],
             ymax_values[idx],
             # alpha=0.5,
-            zorder=5.5,
+            zorder=4.5,
             edgecolor=CB_color_cycle[0],
             linewidth=0.2,
             facecolor="none",
             # facecolor=CB_color_cycle[0],
-            hatch="//",
+            hatch="///",
         )
     ax.add_patch(
         mpatches.Rectangle(
