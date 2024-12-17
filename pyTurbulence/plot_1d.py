1812be9b60a77559182270e39a47ffa07b3a37d0
JonasSuni <jonas.suni@helsinki.fi>
2024-12-17 11:09:54 +0200

    Add plot_elsasser function to visualize Elsasser variables

--- pyTurbulence/plot_1d.py
+++ pyTurbulence/plot_1d.py
@@ -16,6 +16,7 @@
 #     MP_xy,
 # )
 from pyJets.jet_analyser import get_cell_volume, sw_par_dict
+from pyJets.jet_aux import CB_color_cycle
 import pytools as pt
 import os
 import sys
@@ -93,6 +94,49 @@ except:
 wrkdir_DNR = wrkdir_DNR + "turbulence/"
 turbdir = "/wrk-vakka/group/spacephysics/turbulence/"
 
+def plot_elsasser(fnr0,fnr1,dirname):
+
+    figdir = wrkdir_DNR + "Figs/cuts/{}/".format(dirname)
+    if not os.path.exists(figdir):
+        try:
+            os.makedirs(figdir)
+        except OSError:
+            pass
+
+    bulkpath = turbdir + "MultiCircularAlfven/bulk/4_attempt/"
+
+    fnr = np.arange(fnr0, fnr1 + 1)
+
+    for idx in fnr:
+        vlsvobj = pt.vlsvfile.VlsvReader(
+            bulkpath + "bulk." + str(idx).zfill(7) + ".vlsv"
+        )
+        cellids = vlsvobj.read_variable("cellID")
+        ci_sorted = np.sort(cellids)
+        x = np.array([vlsvobj.get_cell_coordinates(c)[0] / r_e for c in ci_sorted])
+        By = vlsvobj.read_variable("vg_b_vol", operator="y")[cellids.argsort()]
+        Bz = vlsvobj.read_variable("vg_b_vol", operator="z")[cellids.argsort()]
+        B = np.array([np.zeros_like(By),By, Bz])
+
+        vy = vlsvobj.read_variable("proton/vg_v", operator="y")[cellids.argsort()]
+        vz = vlsvobj.read_variable("proton/vg_v", operator="z")[cellids.argsort()]
+        v = np.array([np.zeros_like(vy),vy, vz])
+
+        BvA = B / np.sqrt(mu0 * m_p*1e6)
+
+        fig,ax = plt.subplots(1,1,figsize=(8,3),constrained_layout=True)
+
+        ax.plot(x, v+BvA,label="$\\delta z^{+}$",color=CB_color_cycle[0])
+        ax.plot(x, v-BvA,label="$\\delta z^{-}$",color=CB_color_cycle[1])
+
+        ax.set_ylabel(r"$\delta z^{\pm}$")
+        ax.set_xlabel(r"$x~[R_\mathrm{E}]$")
+        ax.set_xlim([x[0], x[-1]])
+        ax.grid()
+        ax.legend()
+
+        fig.savefig(figdir + "{}_elsasser.png".format(idx))
+        plt.close(fig)
 
 def plot_Byz(fnr0, fnr1, dirname):
 
