55e26843d3b4abe73ce89d6a4ef28551f86ae572
JonasSuni <jonas.suni@helsinki.fi>
2025-01-23 14:58:58 +0200

    Fix formatting of axis labels in archerplot function for improved clarity

--- pyJets/agf_jets.py
+++ pyJets/agf_jets.py
@@ -7170,10 +7170,10 @@ def archerplot():
         meds.append([np.nanmedian(xvals), np.nanmedian(yvals)])
 
     ax.set_xlabel(
-        "$\\frac{\\delta\\rho(P_\\mathrm{dyn,max})}{\\langle \\rho \\rangle} / \\frac{\\delta P_\\mathrm{dyn,max}}{\\langle P_\\mathrm{dyn} \\rangle}$"
+        "$\\frac{\\delta\\rho(P_\\mathrm{dyn,max})}{\\langle \\rho \\rangle} \\middle/ \\frac{\\delta P_\\mathrm{dyn,max}}{\\langle P_\\mathrm{dyn} \\rangle}$"
     )
     ax.set_ylabel(
-        "$\\frac{\\delta v^2 (P_\\mathrm{dyn,max})}{\\langle v^2 \\rangle} / \\frac{\\delta P_\\mathrm{dyn,max}}{\\langle P_\\mathrm{dyn} \\rangle}$"
+        "$\\frac{\\delta v^2 (P_\\mathrm{dyn,max})}{\\langle v^2 \\rangle} \\middle/ \\frac{\\delta P_\\mathrm{dyn,max}}{\\langle P_\\mathrm{dyn} \\rangle}$"
     )
     ax.legend()
     ax.axvline(0, linestyle="dashed")
