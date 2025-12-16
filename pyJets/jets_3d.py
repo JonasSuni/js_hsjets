929ba833aa3f83d5957bd623740bfd55f25d8bd6
Jonas Suni <jonas.suni@helsinki.fi>
2025-12-16 18:08:04 +0200

    Fix

--- pyJets/jets_3d.py
+++ pyJets/jets_3d.py
@@ -556,7 +556,7 @@ def L3_good_timeseries_global_vdfs():
 
 def make_timeseries_global_vdf_anim(ci, coords, t0, t1):
 
-    global vdf_axes, cmap_axes, ci_g, x_g, y_g, z_g, axvlines,cmap_cb_ax,vdf_cb_ax
+    global vdf_axes, cmap_axes, ci_g, x_g, y_g, z_g, axvlines, cmap_cb_ax, vdf_cb_ax
 
     x_g, y_g, z_g = coords
 
@@ -569,7 +569,7 @@ def make_timeseries_global_vdf_anim(ci, coords, t0, t1):
             "FIF", coords[0], coords[1], coords[2], 600, 991, None
         )
     )
-    fig = plt.figure(figsize=(8 * 2, 6 * 2), layout="compressed")
+    fig = plt.figure(figsize=(8 * 3, 6 * 3), layout="constrained")
     axes = generate_axes(fig)
     ts_axes = []
     for axname in ["rho", "v", "pdyn", "b", "e", "t"]:
@@ -582,7 +582,7 @@ def make_timeseries_global_vdf_anim(ci, coords, t0, t1):
     generate_ts_plot(ts_axes, ts_data, ci, coords, t0, t1)
     axvlines = []
     for ax in ts_axes:
-        axvlines.append(ax.axvline(t0,linestyle="dashed"))
+        axvlines.append(ax.axvline(t0, linestyle="dashed"))
 
     ani = FuncAnimation(
         fig,
@@ -615,7 +615,7 @@ def ts_glob_vdf_update(fnr):
         pass
     generate_cmap_plots(cmap_axes, vlsvobj)
     for linepl in axvlines:
-        linepl.set_xdata([fnr,fnr])
+        linepl.set_xdata([fnr, fnr])
 
 
 def generate_vdf_plots(vdf_axes, vobj):
@@ -904,21 +904,22 @@ def generate_ts_plot(ts_axes, ts_data, ci, coords, t0, t1):
 #     axes["t"] = fig.add_subplot(gridspec[5:6, 4:8])
 #     return axes
 
+
 def generate_axes(fig):
     gridspec = fig.add_gridspec(nrows=12, ncols=16)
     axes = {}
-    axes['vdf_cb'] = fig.add_subplot(gridspec[0:1, 0:4])
-    axes['vdf_xy'] = fig.add_subplot(gridspec[1:5, 0:4])
-    axes['vdf_xz'] = fig.add_subplot(gridspec[5:9, 0:4])
-    axes['vdf_yz'] = fig.add_subplot(gridspec[9:13, 0:4])
-    axes['cmap_cb'] = fig.add_subplot(gridspec[0:1, 4:8])
-    axes['cmap_xy'] = fig.add_subplot(gridspec[1:5, 4:8])
-    axes['cmap_xz'] = fig.add_subplot(gridspec[5:9, 4:8])
-    axes['cmap_yz'] = fig.add_subplot(gridspec[9:13, 4:8])
-    axes['rho'] = fig.add_subplot(gridspec[0:2, 8:16])
-    axes['v'] = fig.add_subplot(gridspec[2:4, 8:16])
-    axes['pdyn'] = fig.add_subplot(gridspec[4:6, 8:16])
-    axes['b'] = fig.add_subplot(gridspec[6:8, 8:16])
-    axes['e'] = fig.add_subplot(gridspec[8:10, 8:16])
-    axes['t'] = fig.add_subplot(gridspec[10:12, 8:16])
+    axes["vdf_cb"] = fig.add_subplot(gridspec[0:1, 0:4])
+    axes["vdf_xy"] = fig.add_subplot(gridspec[1:5, 0:4])
+    axes["vdf_xz"] = fig.add_subplot(gridspec[5:9, 0:4])
+    axes["vdf_yz"] = fig.add_subplot(gridspec[9:13, 0:4])
+    axes["cmap_cb"] = fig.add_subplot(gridspec[0:1, 4:8])
+    axes["cmap_xy"] = fig.add_subplot(gridspec[1:5, 4:8])
+    axes["cmap_xz"] = fig.add_subplot(gridspec[5:9, 4:8])
+    axes["cmap_yz"] = fig.add_subplot(gridspec[9:13, 4:8])
+    axes["rho"] = fig.add_subplot(gridspec[0:2, 8:16])
+    axes["v"] = fig.add_subplot(gridspec[2:4, 8:16])
+    axes["pdyn"] = fig.add_subplot(gridspec[4:6, 8:16])
+    axes["b"] = fig.add_subplot(gridspec[6:8, 8:16])
+    axes["e"] = fig.add_subplot(gridspec[8:10, 8:16])
+    axes["t"] = fig.add_subplot(gridspec[10:12, 8:16])
     return axes
