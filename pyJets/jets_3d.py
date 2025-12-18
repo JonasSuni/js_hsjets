2211e57e9750c518d1c6cfb1132331170627fa13
Jonas Suni <jonas.suni@helsinki.fi>
2025-12-18 21:01:41 +0200

    Fix

--- pyJets/jets_3d.py
+++ pyJets/jets_3d.py
@@ -598,7 +598,7 @@ def L3_good_timeseries_global_vdfs_one(idx, limitedsize=True, n_processes=16):
     # )
 
     args_list = []
-    fnr_range = np.arange(t0, t1 + 0.1, 1, dtype=int)
+    fnr_range = np.arange(t0[idx], t1[idx] + 0.1, 1, dtype=int)
 
     for idx, fnr in enumerate(fnr_range):
         args_list.append((cellids[idx], coords, t0, t1, fnr, limitedsize, outdir))
