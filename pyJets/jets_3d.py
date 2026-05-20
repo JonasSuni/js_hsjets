13cefe9c0739947f45d38a77570c61c05dfacd2d
JonasSuni <jonas.suni@helsinki.fi>
2026-05-20 14:02:20 +0300

    Plot MS contour instead for jets

--- pyJets/jets_3d.py
+++ pyJets/jets_3d.py
@@ -2028,7 +2028,7 @@ def ext_bs_mp(ax, XmeshXY, YmeshXY, pass_maps):
     rho = pass_maps["proton/vg_rho"]
     pdynx = pass_maps["proton/vg_pdynx"]
 
-    ax.contour(XmeshXY, YmeshXY, rho, [2e6], colors=["red"])
+    # ax.contour(XmeshXY, YmeshXY, rho, [2e6], colors=["red"])
     # try:
     #     ax.contour(XmeshXY, YmeshXY, Tcore, [1.5e6], colors=["red"])
     # except:
@@ -2073,8 +2073,8 @@ def generate_cmap_plots(cmap_axes, vobj, x0, y0, z0, limitedsize):
     cmap_axes[0].axhline(y0, linestyle="dashed", linewidth=0.6, color="k")
     y_arr = np.linspace(y0 - boxwidth, y0 + boxwidth, 100)
     z_arr = np.ones_like(y_arr) * z0
-    x_arr = polyval_bs_at_time(fnr, y_arr, z_arr)
-    cmap_axes[0].plot(x_arr, y_arr, linewidth=0.6, color="k")
+    x_arr = polyval_bs_at_time(fnr, y_arr, z_arr, ms=True)
+    cmap_axes[0].plot(x_arr, y_arr, linewidth=0.6, color="red")
 
     pt.plot.plot_colormap3dslice(
         axes=cmap_axes[1],
@@ -2103,8 +2103,8 @@ def generate_cmap_plots(cmap_axes, vobj, x0, y0, z0, limitedsize):
     cmap_axes[1].axhline(z0, linestyle="dashed", linewidth=0.6, color="k")
     z_arr = np.linspace(z0 - boxwidth, z0 + boxwidth, 100)
     y_arr = np.ones_like(z_arr) * y0
-    x_arr = polyval_bs_at_time(fnr, y_arr, z_arr)
-    cmap_axes[1].plot(x_arr, z_arr, linewidth=0.6, color="k")
+    x_arr = polyval_bs_at_time(fnr, y_arr, z_arr, ms=True)
+    cmap_axes[1].plot(x_arr, z_arr, linewidth=0.6, color="red")
 
     pt.plot.plot_colormap3dslice(
         axes=cmap_axes[2],
@@ -2135,8 +2135,8 @@ def generate_cmap_plots(cmap_axes, vobj, x0, y0, z0, limitedsize):
     y_arr = np.linspace(y0 - boxwidth, y0 + boxwidth, 100)
     z_arr = z_arr = np.linspace(z0 - boxwidth, z0 + boxwidth, 100)
     ymesh, zmesh = np.meshgrid(y_arr, z_arr)
-    xmesh = polyval_bs_at_time(fnr, ymesh, zmesh)
-    cmap_axes[2].contour(ymesh, zmesh, xmesh, [x0], linewidths=[0.6], colors=["k"])
+    xmesh = polyval_bs_at_time(fnr, ymesh, zmesh, ms=True)
+    cmap_axes[2].contour(ymesh, zmesh, xmesh, [x0], linewidths=[0.6], colors=["red"])
 
 
 def generate_ts_plot(ts_axes, ts_data, ci, coords, t0, t1):
