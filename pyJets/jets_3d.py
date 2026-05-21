354afd878ef113561259ee2ff01c5438a5f43c07
JonasSuni <jonas.suni@helsinki.fi>
2026-05-21 14:42:59 +0300

    Test

--- pyJets/jets_3d.py
+++ pyJets/jets_3d.py
@@ -2648,7 +2648,7 @@ def find_bs_cart_ms(vlsvobj, x0, y, z, dr=1000e3, maxiter=1000):
     # Mms = np.abs(np.dot(v, n)) / vms
     Mms = vmag / vf
 
-    print(Mms, v, vmag, B, bmag, vs, va, vms2, costheta, vf)
+    print(vmag, vf, Mms)
 
     while Mms > 1:
         coord = coord + v * dt
@@ -2672,6 +2672,7 @@ def find_bs_cart_ms(vlsvobj, x0, y, z, dr=1000e3, maxiter=1000):
 
         # Mms = np.abs(np.dot(v, n)) / vms
         Mms = vmag / vf
+        print(vmag, vf, Mms)
 
         iter += 1
         if iter > maxiter:
