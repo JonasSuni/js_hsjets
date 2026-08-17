0cc8eb5d21d8f53fbd848ca167acb6bf4cda4b35
JonasSuni <jonas.suni@helsinki.fi>
2026-08-17 11:07:23 +0300

    Fix

--- pyJets/jets_3d.py
+++ pyJets/jets_3d.py
@@ -1139,12 +1139,12 @@ def jet_interval_sorter(runid, len_thresh=1):
             print("Index out of range, exiting gracefully!")
             break
 
+        ci = cellids[idx]
+
         refl = vobj_first.read_variable("vg_reflevel", cellids=int(ci))
         print(refl)
         if refl != 3:
             continue
-
-        ci = cellids[idx]
 
         txtdir = wrkdir_DNR + extrafix + "txts/timeseries/{}".format("")
         ts_data = np.loadtxt(
