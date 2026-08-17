39f88fae82b78cf3a8f6a235df68319b3be5813d
JonasSuni <jonas.suni@helsinki.fi>
2026-08-17 11:36:01 +0300

    Test

--- pyJets/jets_3d.py
+++ pyJets/jets_3d.py
@@ -4022,9 +4022,21 @@ def do_gmm_for_vdf(
         mixture.fit(X)
         print("LL", mixture.score(X))
     else:
-        mixture = GMM(nMaxwellians, weights, means, covs, temperature=temperature)
-        mixture.fit(X, sample_weights, tolerance)
-        print("LL", mixture.loglikelihood)
+        while True:
+            mixture = GMM(nMaxwellians, weights, means, covs, temperature=temperature)
+            mixture.fit(X, sample_weights, tolerance)
+            print("LL", mixture.loglikelihood)
+            ll = mixture.loglikelihood
+            weights, means, covs = mixture.weights, mixture.means, mixture.covs
+            mixture_new = GMM(
+                nMaxwellians, weights, means, covs, temperature=temperature
+            )
+            mixture_new.fit(X, sample_weights, tolerance)
+            ll_new = mixture_new.loglikelihood
+            if ll_new < ll:
+                break
+            else:
+                continue
 
     return mixture
 
